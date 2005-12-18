3e252ffe8dba4afd6246a22340a98eda38c5307e
artemys <julien.boibessot@armadeus.com>
2005-12-18 13:04:25 +0000

    Test to see if SVN is working
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@63 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -58,7 +59,7 @@ logresult()
     fi
 }
 
-echo "******* create dowload folder if required ******* "
+echo "******* create download folder if required ******* "
 cd $INSTALL_DIR
 if [ ! -d $DL_DIR ]; then
 mkdir $DL_DIR
