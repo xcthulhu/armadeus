671e72779c812e5425fb4d89137d6d2e1babdbec
artemys <julien.boibessot@armadeus.com>
2006-03-10 21:54:23 +0000

    Correct patch for 2.6.12 kernel
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@211 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- software/buildroot/package/lcd4linux/lcd4linux.mk
+++ software/buildroot/package/lcd4linux/lcd4linux.mk
@@ -28,7 +28,7 @@ $(LCD4LINUX_DIR)/.unpacked: $(DL_DIR)/$(LCD4LINUX_SOURCE)
 
 $(LCD4LINUX_DIR)/.patched: $(LCD4LINUX_DIR)/.unpacked
 	toolchain/patch-kernel.sh $(LCD4LINUX_DIR) package/lcd4linux $(LCD4LINUX_PATCH)
-	ln -sf $(BUILD_DIR)/linux-2.6.12/include/asm-generic/ $(BUILD_DIR)/staging_dir/lib/gcc/arm-linux-uclibc/sys-include/asm-generic
+	(if [ -d $(BUILD_DIR)/linux-2.6.12/include/asm-generic ]; then ln -sf $(BUILD_DIR)/linux-2.6.12/include/asm-generic/ $(BUILD_DIR)/staging_dir/arm-linux-uclibc/sys-include/asm-generic; fi)
 	touch $(LCD4LINUX_DIR)/.patched
 
 $(LCD4LINUX_DIR)/.configured: $(LCD4LINUX_DIR)/.patched
