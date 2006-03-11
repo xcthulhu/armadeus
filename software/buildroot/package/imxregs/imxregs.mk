5648016dceb731d03f6b8145af93ef6aa9c16dd9
artemys <julien.boibessot@armadeus.com>
2006-03-11 22:27:34 +0000

    First version
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@217 556a79b9-cb07-0410-aa63-ea23e1810fdd

---
+++ software/buildroot/package/imxregs/imxregs.mk
@@ -0,0 +1,45 @@
+#############################################################
+#
+# imxregs debug tool
+#
+#############################################################
+
+IMXREGS_VER:=1.0
+IMXREGS_SOURCE:=$(BUILD_DIR)/../../target/linux/debug/imxregs/
+IMXREGS_DIR:=$(BUILD_DIR)/imxregs-$(IMXREGS_VER)
+IMXREGS_CAT:=zcat
+IMXREGS_BINARY:=imxregs
+IMXREGS_TARGET_BINARY:=usr/bin/imxregs
+
+# Not defined in buildroot ??:
+TARGET_STRIP:=arm-linux-strip
+
+
+$(IMXREGS_DIR)/imxregs.c:
+	mkdir -p $(IMXREGS_DIR)/
+	cp $(IMXREGS_SOURCE)/* $(IMXREGS_DIR)/
+
+$(IMXREGS_DIR)/$(IMXREGS_BINARY): $(IMXREGS_DIR)/imxregs.c
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CC=$(TARGET_CC) -C $(IMXREGS_DIR)
+
+$(TARGET_DIR)/$(IMXREGS_TARGET_BINARY): $(IMXREGS_DIR)/$(IMXREGS_BINARY)
+	install -D $< $@
+	$(TARGET_STRIP) $(TARGET_DIR)/$(IMXREGS_TARGET_BINARY)
+
+imxregs: uclibc $(TARGET_DIR)/$(IMXREGS_TARGET_BINARY)
+
+imxregs-clean:
+	rm -f $(TARGET_DIR)/$(IMXREGS_TARGET_BINARY)
+	-$(MAKE) -C $(IMXREGS_DIR) clean
+
+imxregs-dirclean:
+	rm -rf $(IMXREGS_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_IMXREGS)),y)
+TARGETS+=imxregs
+endif
