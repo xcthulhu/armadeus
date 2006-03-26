3fb26bab959605d6e2fc2eaac6da156712640c98
artemys <julien.boibessot@armadeus.com>
2006-03-26 17:52:09 +0000

    Small modification to set_qt_env.sh
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@226 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- software/buildroot/package/qte_gpl/set_qt_env.sh
+++ software/buildroot/package/qte_gpl/set_qt_env.sh
@@ -4,7 +4,7 @@
 # Environment variables to source when one wants to use Qt/E
 #
 
-export QTDIR=/mnt/host
+export QTDIR=/mnt/host/usr
 export PATH=$QTDIR/bin:$PATH
 export LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
 export QT_THREAD_SUFFIX=-mt
