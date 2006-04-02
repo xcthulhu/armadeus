b8614dc7a506b93aa27bff72901a854bd9b4bf49
artemys <julien.boibessot@armadeus.com>
2006-04-02 12:52:15 +0000

    Add Opie support in set_qt_env.sh
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@235 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- software/buildroot/package/qte_gpl/set_qt_env.sh
+++ software/buildroot/package/qte_gpl/set_qt_env.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 #
-# Environment variables to source when one wants to use Qt/E
+# Environment variables to source when one wants to use Qt/E, Opie, Qtopia & co
 #
 
 export QTDIR=/mnt/host/usr
-export PATH=$QTDIR/bin:$PATH
-export LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
+export OPIEDIR=/mnt/host/usr
+export QPEDIR="$OPIEDIR"
+
+export PATH=$QTDIR/bin:$QPEDIR/bin:$PATH
+export LD_LIBRARY_PATH=$QTDIR/lib:$QPEDIR/lib:$LD_LIBRARY_PATH
 export QT_THREAD_SUFFIX=-mt
 
