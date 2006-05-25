0be304f06a6b60a8d07f4481621a832e38ed33f3
michael <michael.lerjen@armadeus.com>
2006-05-25 16:26:33 +0000

    New version of the PS2 interface, for the first time fully functional for read access.
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@255 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- software/firmware/PS2/PS2_top.vhd
+++ software/firmware/PS2/PS2_top.vhd
@@ -23,6 +23,8 @@ use IEEE.STD_LOGIC_ARITH.all;
 use IEEE.STD_LOGIC_UNSIGNED.all;
 library UNISIM;
 use UNISIM.VComponents.all;
+library XilinxCoreLib;
+use XilinxCoreLib.all;
 
 
 entity PS2_top is                       -- this is the top
@@ -36,112 +38,202 @@ entity PS2_top is                       -- this is the top
          PS2Clk     : inout std_logic;
          PS2Data    : inout std_logic;
          StatOutxDO : out   std_logic
---  --ResetxRB         : in std_logic
          );
 end PS2_top;
 
 
 architecture hierarchical of PS2_top is
--- signal Recv_Ena : std_logic;
--- signal Recv_Sample : std_logic;
-  signal Recv_BRAM_Addr : std_logic_vector(9 downto 0);
+  component fifo_syn
+    generic (
+      width        :     integer;
+      addrdepth    :     integer);
+    port (
+      ClkxC        : in  std_logic;
+      din          : in  std_logic_vector(width-1 downto 0);
+      rd_en        : in  std_logic;
+      ResetxRB     : in  std_logic;
+      wr_en        : in  std_logic;
+      almost_empty : out std_logic;
+      almost_full  : out std_logic;
+      data_count   : out std_logic_vector(addrdepth-1 downto 0);
+      dout         : out std_logic_vector(width-1 downto 0);
+      empty        : out std_logic;
+      full         : out std_logic;
+      overflow     : out std_logic;
+      valid        : out std_logic;
+      underflow    : out std_logic;
+      wr_ack       : out std_logic);
+  end component;
+
   signal Recv_BRAM_Data : std_logic_vector(15 downto 0);
   signal Recv_BRAM_WEN  : std_logic;
 
   signal DataOutRAMxD : std_logic_vector(15 downto 0);
-  signal CSBRAM       : std_logic := '0';
-  signal WRBRAM       : std_logic := '0';
-  signal CLKBRAM      : std_logic := '0';
+  signal resetn       : std_logic;
+  signal reg1         : std_logic_vector(7 downto 0);
+  signal reg2         : std_logic_vector(7 downto 0);
 
+  signal dout         : std_logic_vector(7 downto 0);
+  signal empty        : std_logic;
+  signal full         : std_logic;
+  signal overflow     : std_logic;
+  signal underflow    : std_logic;
+  signal valid        : std_logic;
+  signal almost_empty : std_logic;
+  signal almost_full  : std_logic;
+  signal wr_ack       : std_logic;
+  signal data_count   : std_logic_vector(3 downto 0);
+
+  signal readaccess          : std_logic;
+  signal readAccessPulse     : std_logic;
+  signal readAccessPulseFiFo : std_logic;
+  
 begin
---
---
---  -- RAMB16_S18: Virtex-II/II-Pro, Spartan-3 1k x 16 + 2 Parity bits Single-Port RAM
---  -- Xilinx  HDL Language Template version 6.3.1i
---
-  RAMB16_D18_18_inst : RAMB16_S18_S18
-    generic map (
-      INIT_A  => X"00000",              --  Value of output RAM registers at startup
-      SRVAL_A => X"00000",              --  Ouput value upon SSR assertion
-      INIT_00 => X"1100001100000000000000000000000000000000000000000000000010010110"
-      )
-    port map (
-      DOA     => DataOutRAMxD,          -- 16-bit Data Output
-      DOPA    => open,                  -- 2-bit parity Output
-      ADDRA   => AddrxDI,               -- 10-bit Address Input
-      CLKA    => CLKBRAM,               -- Clock
-      DIA     => DataxD,                -- 16-bit Data Input
-      DIPA    => "00",                  -- 2-bit parity Input
-      ENA     => CSBRAM,                -- RAM Enable Input
-      SSRA    => '0',                   -- Synchronous Set/Reset Input
-      WEA     => WRBRAM,                -- Write Enable Input
 
-      DOB   => open,                    -- 16-bit Data Output
-      DOPB  => open,                    -- 2-bit parity Output
-      ADDRB => Recv_BRAM_Addr,          -- 10-bit Address Input
-      CLKB  => CLKBRAM,                 -- Clock
-      DIB   => Recv_BRAM_Data,          -- 16-bit Data Input
-      DIPB  => "00",                    -- 2-bit parity Input
-      ENB   => '1',                     -- RAM Enable Input
-      SSRB  => '0',                     -- Synchronous Set/Reset Input
-      WEB   => Recv_BRAM_WEN            -- Write Enable Input
-
-      );
-
-  -- End of RAMB16_18_18_inst instantiation
+  resetn <= '1';
 
 
-  OutputEnable : process (DataOutRAMxD, CSxBI, RDxBI)
-  begin
-    if (CSxBI = '0' and RDxBI = '0') then  -- fix me
-      DataxD <= DataOutRAMxD;
-    else
-      DataxD <= (others => 'Z');           --
+  -----------------------------------------------------------------------------
+  -- Process
+  -- purpose: Register Write from businterface
+  -----------------------------------------------------------------------------
+  PS2register_write : process (ClkxCI, resetn)
+  begin  -- process PS2register_write
+    if resetn = '0' then                -- asynchronous reset (active low)
+      reg1       <= "10000000";
+      reg2       <= (others => '0');
+    elsif ClkxCI'event and ClkxCI = '1' then  -- rising clock edge
+      if CSxBI = '0' and WRxBI = '0' then
+        case AddrxDI is
+          when "0000000010" =>
+            reg1 <= DataxD(7 downto 0);
+          when "0000000011" =>
+            reg2 <= DataxD(7 downto 0);
+          when others       => null;
+        end case;
+      end if;
     end if;
-  end process OutputEnable;
-
-  CSBRAM  <= not CSxBI;
-  WRBRAM  <= not WRxBI;
-  CLKBRAM <= ClkxCI;
+  end process PS2register_write;
 
 
-  -- purpose: checks start and stop of reception
-  -- type   : sequential
-  -- inputs : ClkxCI, CSxBI
-  -- outputs: 
-  Recv_FSM                 : process (ClkxCI, CSxBI)
+
+  -----------------------------------------------------------------------------
+  -- Process
+  -- purpose: Generic Bus-Interface for read access
+  -----------------------------------------------------------------------------
+  PS2register_read_syn            : process (ClkxCI, resetn)
+    variable ReadAccessEdgeDetect : std_logic;
+  begin  -- process PS2register_read_syn
+    if resetn = '0' then                -- asynchronous reset (active low)
+      readaccess        <= '0';
+      ReadAccessPulse   <= '1';
+      ReadAccessEdgeDetect := '0';
+      DataOutRAMxD      <= (others => 'Z');
+    elsif ClkxCI'event and ClkxCI = '1' then  -- rising clock edge
+      if (ReadAccessEdgeDetect = '0' and ((not CSxBI) and (not RDxBI)) = '1') then
+        ReadAccessPulse <= '0';
+      else
+        ReadAccessPulse <= '1';
+      end if;
+      ReadAccessEdgeDetect := (not CSxBI) and (not RDxBI);
+    end if;
+  end process PS2register_read_syn;
+
+
+  
+  -----------------------------------------------------------------------------
+  -- Process
+  -- purpose: Register Read from businterface
+  -----------------------------------------------------------------------------
+  PS2register_read : process ( CSxBI, RDxBI, AddrxDI, reg1, reg2, readAccessPulse, 
+                               Recv_BRAM_Data, valid, overflow, full, almost_full, empty, almost_empty, dout, data_count)
+  begin  -- process PS2register_read
+    readAccessPulseFiFo <= '1';
+    if CSxBI = '0' and RDxBI = '0' then
+      case AddrxDI is
+        when "0000000000" =>
+          DataOutRAMxD <= "00" & data_count(1 downto 0) & "0000" & "00" & valid & overflow & full & almost_full & empty & almost_empty;
+        when "0000000001" =>
+          DataOutRAMxD <= "00000000" & dout;
+          if readAccessPulse = '0' then
+            readAccessPulseFiFo <= '0';
+          end if;
+        when "0000000010" =>
+          DataOutRAMxD <= "00000000" & reg1;
+        when "0000000011" =>
+          DataOutRAMxD <= "00000000" & reg2;
+
+        when "0000000100"         =>
+          DataOutRAMxD <= "0001001000110100";
+        when others               =>
+          DataOutRAMxD <= (others => '0');
+      end case;
+    else
+      DataOutRAMxD     <= (others => 'Z');
+    end if;
+  end process PS2register_read;
+--
+  DataxD               <= DataOutRAMxD;
+
+
+  -----------------------------------------------------------------------------
+  -- FIFO Instantiation
+  -----------------------------------------------------------------------------
+  ps2_read_fifo : fifo_syn
+    generic map (
+      width        => 8,
+      addrdepth    => 4)
+    port map (
+      ClkxC        => ClkxCI,
+      din          => Recv_BRAM_Data(7 downto 0),
+      rd_en        => ReadAccessPulseFiFo,  --rd_en,
+      ResetxRB     => resetn,
+      wr_en        => Recv_BRAM_WEN,
+      almost_empty => almost_empty,
+      almost_full  => almost_full,
+      data_count   => data_count,
+      dout         => dout,
+      empty        => empty,
+      full         => full,
+      overflow     => overflow,
+      valid        => valid,
+      underflow    => underflow,
+      wr_ack       => wr_ack);
+
+
+  -----------------------------------------------------------------------------
+  -- Process
+  -- purpose: PS2 receiver
+  -----------------------------------------------------------------------------
+  Recv_FSM                 : process (ClkxCI, resetn)
     variable bitcounter    : integer                       := 0;
-    variable wordcounter   : std_logic_vector(9 downto 0)  := (others => '0');
     variable edge_detect_v : std_logic_vector(2 downto 0)  := "000";
     variable recv_buffer   : std_logic_vector(11 downto 0) := (others => '0');
   begin  -- process Recv_FSM     
 
-    if CSxBI = '0' then                 -- asynchronous reset (active low)
-      bitcounter    := 0;
-      wordcounter   := (others    => '0');
-      edge_detect_v := "000";
-      Recv_BRAM_WEN    <= '0';
+    if resetn = '0' then                -- asynchronous reset (active low)
+      bitcounter     := 0;
+      edge_detect_v  := "000";
+      Recv_BRAM_WEN    <= '1';
     elsif ClkxCI'event and ClkxCI = '1' then  -- rising clock edge
-      Recv_BRAM_WEN    <= '0';
-      Recv_BRAM_Data   <= (others => '0');
-      StatOutxDO       <= '0';
+      Recv_BRAM_WEN    <= '1';
       if edge_detect_v = "110" and PS2Clk = '0' then  ---and PS2Data = '0' then  -- = "110"
-        recv_buffer := PS2Data & recv_buffer(11 downto 1);
-        bitCounter  := bitCounter + 1;
-        StatOutxDO     <= '1';
+        recv_buffer  := PS2Data & recv_buffer(11 downto 1);
+        bitCounter   := bitCounter + 1;
       elsif bitcounter = 11 then
-        Recv_BRAM_WEN  <= '1';
-        bitCounter  := bitcounter + 1;
+        Recv_BRAM_WEN  <= '0';
         Recv_BRAM_Data <= "00000" & recv_buffer(1) & recv_buffer(10) & recv_buffer(11) & recv_buffer(9 downto 2);
+        if wr_ack = '1' then
+          bitCounter := bitcounter + 1;
+        end if;
       elsif bitcounter = 12 then
-        bitcounter  := 0;
-        wordCounter := wordCounter + 1;
+        bitcounter   := 0;
       end if;
-      edge_detect_v := edge_detect_v(1 downto 0) & PS2Clk;
+      edge_detect_v  := edge_detect_v(1 downto 0) & PS2Clk;
 
     end if;
-    Recv_BRAM_Addr <= wordCounter;
 
   end process Recv_FSM;
 
+  StatOutxDO <= wr_ack;
 end hierarchical;
