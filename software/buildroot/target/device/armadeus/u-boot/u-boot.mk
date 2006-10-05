bbea6f9c6484fd9ee7e35766b3d6d43f5e047c89
jorasse <eric.jarrige@armadeus.org>
2006-10-05 21:26:29 +0000

    adapt MTDMAP to the target
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@374 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- software/buildroot/target/device/armadeus/u-boot/u-boot.mk
+++ software/buildroot/target/device/armadeus/u-boot/u-boot.mk
@@ -35,12 +35,16 @@ ifneq ($(BR2_TARGET_ARMADEUS_APF9328),)
 		$(U-BOOT_DIR)/include/configs/$(U-BOOT_TARGET_NAME).h
 	$(SED) 's,^#define DM9000_DATA.*,#define DM9000_DATA		(CONFIG_DM9000_BASE+2),g' \
 		$(U-BOOT_DIR)/include/configs/$(U-BOOT_TARGET_NAME).h
+	$(SED) 's,^#define CONFIG_MTDMAP.*,#define CONFIG_MTDMAP  "apf9328_flash",g' \
+		$(U-BOOT_DIR)/include/configs/$(U-BOOT_TARGET_NAME).h
 else
 	$(SED) 's,^#define CONFIG_MACH_TYPE.*,#define CONFIG_MACH_TYPE MACH_TYPE_APM9328,g' \
 		$(U-BOOT_DIR)/include/configs/$(U-BOOT_TARGET_NAME).h
 	$(SED) 's,^#define CONFIG_DM9000_BASE.*,#define CONFIG_DM9000_BASE    0x15C3FFFC,g' \
 		$(U-BOOT_DIR)/include/configs/$(U-BOOT_TARGET_NAME).h
 	$(SED) 's,^#define DM9000_DATA.*,#define DM9000_DATA		(CONFIG_DM9000_BASE+4),g' \
+		$(U-BOOT_DIR)/include/configs/$(U-BOOT_TARGET_NAME).h
+	$(SED) 's,^#define CONFIG_MTDMAP.*,#define CONFIG_MTDMAP  "scb9328_flash",g' \
 		$(U-BOOT_DIR)/include/configs/$(U-BOOT_TARGET_NAME).h
 endif
 	touch $(U-BOOT_DIR)/.configured	
