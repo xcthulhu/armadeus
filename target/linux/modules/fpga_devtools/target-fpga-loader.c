854aee0b0ee26b69954f79013e6eed148dde704d
thom25 <nicolas.colombain@armadeus.com>
2006-12-09 14:23:32 +0000

    fix bug when initializing the gpios imx_regs.h/gpiomode function from uboot are not equivalent to the one from linux !!!!
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@505 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- target/linux/modules/fpga_devtools/target-fpga-loader.c
+++ target/linux/modules/fpga_devtools/target-fpga-loader.c
@@ -26,8 +26,6 @@
 #include <asm/arch/hardware.h>
 #include <asm/io.h>
 
-#define GPIO_GPIO GPIO_OCR_MASK // gpio mode
-
 #define GPIO_PORT(x)  ((x >> 5) & 3)
 #define GPIO_SET(x)   (DR(GPIO_PORT(x)) |= (1<<(x & GPIO_PIN_MASK)))
 #define GPIO_CLEAR(x) (DR(GPIO_PORT(x)) &= ~(1<<(x & GPIO_PIN_MASK)))
@@ -80,12 +78,12 @@ int fpga_wr_fn( int assert_write )
 
 int fpga_pre_fn( void )
 {
-	// Initialize GPIO pins
-	imx_gpio_mode (FPGA_INIT | GPIO_GPIO | GPIO_IN );
-	imx_gpio_mode (FPGA_DONE | GPIO_GPIO | GPIO_IN );
-	imx_gpio_mode (FPGA_DIN  | GPIO_GPIO | GPIO_OUT );
-	imx_gpio_mode (FPGA_PROGRAM | GPIO_GPIO | GPIO_OUT );
-	imx_gpio_mode (FPGA_CLOCK | GPIO_GPIO | GPIO_OUT );
+    // Initialize GPIO pins
+	imx_gpio_mode (FPGA_INIT | GPIO_GIUS | GPIO_DR | GPIO_IN );
+	imx_gpio_mode (FPGA_DONE | GPIO_GIUS | GPIO_DR | GPIO_IN );
+	imx_gpio_mode (FPGA_DIN  | GPIO_GIUS | GPIO_DR | GPIO_OUT );
+	imx_gpio_mode (FPGA_PROGRAM | GPIO_GIUS | GPIO_DR | GPIO_OUT );
+	imx_gpio_mode (FPGA_CLOCK | GPIO_GIUS | GPIO_DR | GPIO_OUT );
 	return 1;
 }
 
