dda1b4b8f1981e40fdc4593f26b63be8b346410a
thom25 <nicolas.colombain@armadeus.com>
2006-12-15 19:32:57 +0000

    add fpgaregs to buildroot
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@513 556a79b9-cb07-0410-aa63-ea23e1810fdd

---
+++ buildroot/package/fpgaregs/fpgaregs.mk
@@ -0,0 +1,41 @@
+#############################################################
+#
+# fpgaregs debug tool
+#
+#############################################################
+
+FPGAREGS_VER:=1.0
+FPGAREGS_SOURCE:=$(BUILD_DIR)/../../target/linux/debug/fpgaregs/
+FPGAREGS_DIR:=$(BUILD_DIR)/fpgaregs-$(FPGAREGS_VER)
+FPGAREGS_CAT:=zcat
+FPGAREGS_BINARY:=fpgaregs
+FPGAREGS_TARGET_BINARY:=usr/bin/fpgaregs
+
+$(FPGAREGS_DIR)/fpgaregs.c:
+	mkdir -p $(FPGAREGS_DIR)/
+	cp $(FPGAREGS_SOURCE)/* $(FPGAREGS_DIR)/
+
+$(FPGAREGS_DIR)/$(FPGAREGS_BINARY): $(FPGAREGS_DIR)/fpgaregs.c
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CC=$(TARGET_CC) -C $(FPGAREGS_DIR)
+
+$(TARGET_DIR)/$(FPGAREGS_TARGET_BINARY): $(FPGAREGS_DIR)/$(FPGAREGS_BINARY)
+	install -D $< $@
+	$(STRIP) $(TARGET_DIR)/$(FPGAREGS_TARGET_BINARY)
+
+fpgaregs: uclibc $(TARGET_DIR)/$(FPGAREGS_TARGET_BINARY)
+
+fpgaregs-clean:
+	rm -f $(TARGET_DIR)/$(FPGAREGS_TARGET_BINARY)
+	-$(MAKE) -C $(FPGAREGS_DIR) clean
+
+fpgaregs-dirclean:
+	rm -rf $(FPGAREGS_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_FPGAREGS)),y)
+TARGETS+=fpgaregs
+endif
