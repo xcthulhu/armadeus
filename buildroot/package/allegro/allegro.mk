f2a992e6479431a64fe9143f773c69b344f27537
artemys <julien.boibessot@armadeus.com>
2007-05-11 16:40:58 +0000

    [BUILDROOT] First step in Allegro integration
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@596 556a79b9-cb07-0410-aa63-ea23e1810fdd

---
+++ buildroot/package/allegro/allegro.mk
@@ -0,0 +1,75 @@
+#############################################################
+#
+# Allegro: a game programming library
+#
+#############################################################
+ALLEGRO_VERSION:=4.2.1
+ALLEGRO_SOURCE:=allegro-$(ALLEGRO_VERSION).tar.gz
+ALLEGRO_SITE:=http://prdownloads.sourceforge.net/alleg/
+ALLEGRO_CAT:=zcat
+ALLEGRO_DIR:=$(BUILD_DIR)/allegro-$(ALLEGRO_VERSION)
+
+$(DL_DIR)/$(ALLEGRO_SOURCE):
+	$(WGET) -P $(DL_DIR) $(ALLEGRO_SITE)/$(ALLEGRO_SOURCE)?download
+
+allegro-source: $(DL_DIR)/$(ALLEGRO_SOURCE)
+
+$(ALLEGRO_DIR)/.unpacked: $(DL_DIR)/$(ALLEGRO_SOURCE)
+	$(ALLEGRO_CAT) $(DL_DIR)/$(ALLEGRO_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	touch $@
+
+$(ALLEGRO_DIR)/.configured: $(ALLEGRO_DIR)/.unpacked
+	(cd $(ALLEGRO_DIR); \
+	$(TARGET_CONFIGURE_OPTS) \
+	CFLAGS="$(TARGET_CFLAGS)" \
+	LDFLAGS="$(TARGET_LDFLAGS)" \
+	CROSSCOMPILE=1 \
+	./configure \
+		--target=$(GNU_TARGET_NAME) \
+		--host=$(GNU_TARGET_NAME) \
+		--build=$(GNU_HOST_NAME) \
+		--prefix=/usr \
+		--without-svgalib \
+		--enable-modules=no \
+		--enable-static=yes \
+		--enable-shared=no \
+		--disable-asm \
+		--disable-mmx \
+		--disable-sse \
+		--disable-color24 \
+		--disable-color32 \
+		--without-x );
+	touch $@
+# allegro doesn't seem to build as shared library
+# 		--disable-color24 \
+# 		--disable-color32 \
+
+$(ALLEGRO_DIR)/.compiled: $(ALLEGRO_DIR)/.configured
+	$(MAKE) -C $(ALLEGRO_DIR) CROSSCOMPILE=1
+	touch $@
+
+# $(STAGING_DIR)/usr/lib/libSDL_net.so: $(ALLEGRO_DIR)/.compiled
+# 	$(MAKE) -C $(ALLEGRO_DIR) install
+# 	touch -c $@
+# 
+# $(TARGET_DIR)/usr/lib/libSDL_net.so: $(STAGING_DIR)/usr/lib/libSDL_net.so
+# 	cp -dpf $(STAGING_DIR)/usr/lib/libSDL_net*.so* $(TARGET_DIR)/usr/lib/
+# 	-$(STRIP) --strip-unneeded $(TARGET_DIR)/usr/lib/libSDL_net.so
+
+ALLEGRO allegro: uclibc $(ALLEGRO_DIR)/.compiled
+
+allegro-clean:
+	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(ALLEGRO_DIR) uninstall
+	-$(MAKE) -C $(ALLEGRO_DIR) clean
+ 
+allegro-dirclean:
+	rm -rf $(ALLEGRO_DIR)
+ 
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_ALLEGRO)),y)
+TARGETS+=allegro
+endif
