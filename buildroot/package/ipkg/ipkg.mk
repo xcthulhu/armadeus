95ff302789a95c4d9e5d8e4bc0dfd235c690f2b8
artemys <julien.boibessot@armadeus.com>
2007-08-09 17:14:12 +0000

    [BUILDROOT] Add package generation of ipkg util
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@618 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- buildroot/package/ipkg/ipkg.mk
+++ buildroot/package/ipkg/ipkg.mk
@@ -9,10 +9,11 @@ IPKG_VERSION:=0.99.149
 IPKG_RELEASE:=2
 IPKG_MD5SUM:=975cc419d6db5fb279dc58177c68373b
 
-# IPKG_SOURCE_URL:=http://www.handhelds.org/packages/ipkg \
-# 	http://www.gtlib.gatech.edu/pub/handhelds.org/packages/ipkg \
-# 	http://ftp.gwdg.de/pub/linux/handhelds/packages/ipkg
 IPKG_SITE:=http://ftp.gwdg.de/pub/linux/handhelds/packages/ipkg
+# Mirrors:
+# http://www.handhelds.org/packages/ipkg
+# http://www.gtlib.gatech.edu/pub/handhelds.org/packages/ipkg
+# http://ftp.gwdg.de/pub/linux/handhelds/packages/ipkg
 IPKG_SOURCE:=$(IPKG_NAME)-$(IPKG_VERSION).tar.gz
 IPKG_CAT:=zcat
 
@@ -34,7 +35,14 @@ $(IPKG_BUILD_DIR)/.patched: $(IPKG_BUILD_DIR)/.unpacked
 	toolchain/patch-kernel.sh $(IPKG_BUILD_DIR) package/ipkg/patches \*.patch
 	touch $@
 
-$(IPKG_BUILD_DIR)/.configured: $(IPKG_BUILD_DIR)/.patched
+$(IPKG_BUILD_DIR)/ipkg/$(IPKG_NAME)/CONTROL/control: $(IPKG_BUILD_DIR)/.patched
+	scripts/make-ipkg-dir.sh $(IPKG_BUILD_DIR)/ipkg/$(IPKG_NAME) package/$(IPKG_NAME)/ipkg/$(IPKG_NAME).control $(IPKG_VERSION) arm
+	if [ "$$(IDEPEND_$(1))" != "" ]; then echo "Depends: $$(IDEPEND_$(1))" >> $(IPKG_BUILD_DIR)/ipkg/$(IPKG_NAME)/CONTROL/control; fi
+	for file in conffiles preinst postinst prerm postrm; do \
+		[ -f ./ipkg/$(IPKG_NAME).$$$$file ] && cp ./ipkg/$(IPKG_NAME).$$$$file $(IPKG_BUILD_DIR)/ipkg/$(IPKG_NAME)/CONTROL/$$$$file || true; \
+	done
+
+$(IPKG_BUILD_DIR)/.configured: $(IPKG_BUILD_DIR)/ipkg/$(IPKG_NAME)/CONTROL/control
 	(cd $(IPKG_BUILD_DIR); rm -rf config.{cache,status} ; \
 		$(TARGET_CONFIGURE_OPTS) \
 		CFLAGS="$(TARGET_CFLAGS)" \
@@ -72,12 +80,25 @@ $(IPKG_BUILD_DIR)/.compiled: $(IPKG_BUILD_DIR)/.configured
 		all install
 	touch $@
 
+IPKG := IPKG_INSTROOT=$(TARGET_DIR) IPKG_CONF_DIR=$(TARGET_DIR)/../ scripts/ipkg -force-defaults -force-depends
+# invoke ipkg-build with some default options
+TARGET_PATH=$(STAGING_DIR)/usr/bin:$(STAGING_DIR)/bin:/bin:/sbin:/usr/bin:/usr/sbin
+IPKG_BUILD := PATH="$(TARGET_PATH)" ipkg-build -c -o root -g root
+# Define it where STAGING_DIR & Co is done -->>
+PACKAGE_DIR:= $(BUILD_DIR)/../bin/packages
+
 ipkg-c: uclibc $(IPKG_BUILD_DIR)/.compiled
 	cp $(IPKG_INSTALL_DIR)/bin/ipkg-cl $(TARGET_DIR)/bin/ipkg
 	-$(STRIP) --strip-unneeded $(IPKG_INSTALL_DIR)/usr/lib/libipkg.so*
 	cp -P $(IPKG_INSTALL_DIR)/usr/lib/libipkg.so* $(TARGET_DIR)/usr/lib/
+	# ipkg stuff:
+	mkdir -p $(IPKG_BUILD_DIR)/ipkg/$(IPKG_NAME)/bin
+	mkdir -p $(IPKG_BUILD_DIR)/ipkg/$(IPKG_NAME)/usr/lib
+	cp $(TARGET_DIR)/bin/ipkg $(IPKG_BUILD_DIR)/ipkg/$(IPKG_NAME)/bin/
+	cp -P $(IPKG_INSTALL_DIR)/usr/lib/libipkg.so* $(IPKG_BUILD_DIR)/ipkg/$(IPKG_NAME)/usr/lib
+#	$(IPKG) install bin/packages/$(IPKG_NAME)-$(IPKG_VERSION)-arm.ipk
 #	$(RSTRIP) $(IDIR_IPKG_C)
-#	$(IPKG_BUILD) $(IDIR_IPKG_C) $(PACKAGE_DIR)
+	$(IPKG_BUILD) $(IPKG_BUILD_DIR)/ipkg/$(IPKG_NAME) $(PACKAGE_DIR)
 	
 ipkg-sh: uclibc
 	install -m0755 ./package/ipkg/files/ipkg $(TARGET_DIR)/bin/ipkg
