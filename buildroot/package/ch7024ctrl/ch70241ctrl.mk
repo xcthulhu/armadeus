4a8253480883a97a7aa69e6984da3af6b6d8153d
thom25 <nicolas.colombain@armadeus.com>
2007-09-02 18:20:37 +0000

    add ch7024 controller
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@631 556a79b9-cb07-0410-aa63-ea23e1810fdd

---
+++ buildroot/package/ch7024ctrl/ch70241ctrl.mk
@@ -0,0 +1,42 @@
+#############################################################
+#
+# ch7024 tool
+#
+#############################################################
+
+CH7024CTRL_VER:=1.0
+CH7024CTRL_SOURCE:=$(BUILD_DIR)/../../target/packages/ch7024ctrl
+CH7024CTRL_DIR:=$(BUILD_DIR)/ch7024ctrl-$(CH7024CTRL_VER)
+CH7024CTRL_CAT:=zcat
+CH7024CTRL_BINARY:=ch7024
+CH7024CTRL_TARGET_BINARY:=usr/bin/ch7024
+
+$(CH7024CTRL_DIR)/ch7024.c:
+	set
+	mkdir -p $(CH7024CTRL_DIR)/
+	cp $(CH7024CTRL_SOURCE)/* $(CH7024CTRL_DIR)/
+
+$(CH7024CTRL_DIR)/$(CH7024CTRL_BINARY): $(CH7024CTRL_DIR)/ch7024.c $(CH7024CTRL_DIR)/ch7024.h
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CC=$(TARGET_CC) -C $(CH7024CTRL_DIR)
+
+$(TARGET_DIR)/$(CH7024CTRL_TARGET_BINARY): $(CH7024CTRL_DIR)/$(CH7024CTRL_BINARY)
+	install -D $< $@
+	$(STRIP) $(TARGET_DIR)/$(CH7024CTRL_TARGET_BINARY)
+
+ch7024ctrl: uclibc $(TARGET_DIR)/$(CH7024CTRL_TARGET_BINARY)
+
+ch7024ctrl-clean:
+	rm -f $(TARGET_DIR)/$(CH7024CTRL_TARGET_BINARY)
+	-$(MAKE) -C $(CH7024CTRL_DIR) clean
+
+ch7024ctrl-dirclean:
+	rm -rf $(CH7024CTRL_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(strip $(BR2_PACKAGE_CH7024CTRL)),y)
+TARGETS+=ch7024ctrl
+endif
