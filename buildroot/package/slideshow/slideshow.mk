add8c9101978e4c2bf18ae6b66ebed3e8c099dc4
artemys <julien.boibessot@armadeus.com>
2007-09-07 21:50:01 +0000

    [BUILDROOT] Slideshow tool is now installed in rootfs when built
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@639 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- buildroot/package/slideshow/slideshow.mk
+++ buildroot/package/slideshow/slideshow.mk
@@ -7,7 +7,7 @@ SLIDESHOW_VERSION:=1.1
 SLIDESHOW_SOURCE:=slideshow-source-$(SLIDESHOW_VERSION).zip
 SLIDESHOW_SITE:=http://www.xmission.com/~jstanley/
 SLIDESHOW_CAT:=zcat
-SLIDESHOW_DIR:=$(BUILD_DIR)/slideshow/
+SLIDESHOW_DIR:=$(BUILD_DIR)/slideshow
 
 $(DL_DIR)/$(SLIDESHOW_SOURCE):
 	$(WGET) -P $(DL_DIR) $(SLIDESHOW_SITE)/$(SLIDESHOW_SOURCE)
@@ -23,25 +23,17 @@ $(SLIDESHOW_DIR)/.configured: $(SLIDESHOW_DIR)/.unpacked
 	toolchain/patch-kernel.sh $(SLIDESHOW_DIR) package/slideshow/ slideshow-\*.patch
 	touch $@
 
-$(SLIDESHOW_DIR)/src/slideshow: $(SLIDESHOW_DIR)/.configured
+$(SLIDESHOW_DIR)/slideshow: $(SLIDESHOW_DIR)/.configured
 	export PATH=$(STAGING_DIR)/bin::$$PATH; $(MAKE) -C $(SLIDESHOW_DIR) ARCH=arm
 
-$(STAGING_DIR)/usr/bin/slideshow: $(SLIDESHOW_DIR)/src/slideshow
-	$(MAKE) -C $(SLIDESHOW_DIR) install
+$(TARGET_DIR)/usr/bin/slideshow: $(SLIDESHOW_DIR)/slideshow
+	cp -dpf $< $(TARGET_DIR)/usr/bin
+	-$(STRIP) --strip-unneeded $@
 
-# $(TARGET_DIR)/usr/games/prboom: $(STAGING_DIR)/usr/games/$(GNU_TARGET_NAME)-prboom
-# 	mkdir -p $(TARGET_DIR)/usr/games/
-# 	mkdir -p $(TARGET_DIR)/usr/share/games/doom/
-# 	cp -dpf $(STAGING_DIR)/usr/games/$(GNU_TARGET_NAME)-prboom $@
-# 	cp -dpf $(SLIDESHOW_DIR)/data/*.wad $(TARGET_DIR)/usr/share/games/doom/ 
-# 	-$(STRIP) --strip-unneeded $@
-
-SLIDESHOW slideshow: sdl sdl_image $(STAGING_DIR)/usr/bin/slideshow
+SLIDESHOW slideshow: sdl sdl_image $(TARGET_DIR)/usr/bin/slideshow
 
 slideshow-clean:
-#	$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(SLIDESHOW_DIR) uninstall
-#	rm -rf $(TARGET_DIR)/usr/share/games/doom/
-#	rm -rf $(TARGET_DIR)/usr/games/
+	rm -f $(TARGET_DIR)/usr/bin/slideshow
 	-$(MAKE) -C $(SLIDESHOW_DIR) clean
  
 slideshow-dirclean:
