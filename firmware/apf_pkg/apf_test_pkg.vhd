9bac1819e85972fd646a5421689fee55f842453d
FabM <fabien.marteau@armadeus.com>
2008-04-02 15:07:16 +0000

    [FIRMWARE][FPGA] correct some mistakes, and adding GPL
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@786 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- firmware/apf_pkg/apf_test_pkg.vhd
+++ firmware/apf_pkg/apf_test_pkg.vhd
@@ -1,11 +1,27 @@
-
 ----------------------------------------------
 -- Design Name : Test bench utils
--- File Name : apf_test_pkg.vhw
+-- File Name : apf_test_pkg.vhd
 -- Function : Defines communication function between imx and fpga
--- Author   : Fabien Marteau
+-- Author   : Fabien Marteau <fabien.marteau@armadeus.com>
 -- Version  : 1.00
 ---------------------------------------------
+-----------------------------------------------------------------------------------
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2, or (at your option)
+-- any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+----------------------------------------------------------------------------------
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 
--- firmware/bus_led/testbench/led_top_tb.vhd
+++ firmware/bus_led/testbench/led_top_tb.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company     : ARMadeus Systems
--- Author(s)   : Fabien Marteau
+-- Author(s)   : Fabien Marteau <fabien.marteau@armadeus.com>
 -- 
 -- Creation Date : 14/02/2008
 -- File          : led_top_tb.vhd
@@ -8,6 +8,22 @@
 -- Abstract : Testbench for simple design that switch a led with the imx
 --
 ----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2, or (at your option)
+-- any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+----------------------------------------------------------------------------------
+
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
