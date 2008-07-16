b75863c6b0c954bf202d52c26f4d57e49ea17330
artemys <julien.boibessot@armadeus.com>
2008-07-16 09:31:19 +0000

    [BUILDROOT] Add Socket CAN package (not activated by menuconfig yet)
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@829 556a79b9-cb07-0410-aa63-ea23e1810fdd

---
+++ buildroot/package/socketcan/socketcan.mk
@@ -0,0 +1,60 @@
+#############################################################
+#
+# Socket CAN
+#
+#############################################################
+SOCKETCAN_TRUNK:=http://svn.berlios.de/svnroot/repos/socketcan/trunk
+SOCKETCAN_REVISION:=810
+SOCKETCAN_NAME:=socketcan-svn-rev$(SOCKETCAN_REVISION)
+SOCKETCAN_DIR:=$(BUILD_DIR)/$(SOCKETCAN_NAME)
+SOCKETCAN_SOURCE:=$(SOCKETCAN_NAME).tar.bz2
+SOCKETCAN_CAT=$(BZCAT)
+SOCKETCAN_EXE=candump
+
+$(DL_DIR)/$(SOCKETCAN_SOURCE):
+	(cd $(BUILD_DIR); \
+		$(SVN_CO) -r $(SOCKETCAN_REVISION) $(SOCKETCAN_TRUNK) $(SOCKETCAN_NAME); \
+		tar -cvf $(SOCKETCAN_NAME).tar $(SOCKETCAN_NAME); \
+		bzip2 $(SOCKETCAN_NAME).tar; \
+		rm -rf $(SOCKETCAN_DIR); \
+		mv $(SOCKETCAN_SOURCE) $(DL_DIR)/$(SOCKETCAN_SOURCE); \
+	)
+
+$(SOCKETCAN_DIR)/.unpacked: $(DL_DIR)/$(SOCKETCAN_SOURCE)
+	$(SOCKETCAN_CAT) $(DL_DIR)/$(SOCKETCAN_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	toolchain/patch-kernel.sh $(SOCKETCAN_DIR) package/socketcan $(SOCKETCAN_NAME)\*.patch	
+	touch $@
+
+$(SOCKETCAN_DIR)/.configured: $(SOCKETCAN_DIR)/.unpacked
+	touch $@
+
+$(SOCKETCAN_DIR)/.compiled: $(SOCKETCAN_DIR)/.configured
+	$(MAKE) CC=$(TARGET_CC) -C $(SOCKETCAN_DIR)/can-utils
+	touch $@
+
+$(TARGET_DIR)/usr/bin/$(SOCKETCAN_EXE): $(SOCKETCAN_DIR)/.compiled
+	$(MAKE) STRIP_CMD="$(STRIPCMD) $(STRIP_STRIP_UNNEEDED)" -C $(SOCKETCAN_DIR)/can-utils strip
+	$(MAKE) INSTALL_DIR=$(TARGET_DIR)/usr/bin -C $(SOCKETCAN_DIR)/can-utils install
+	touch $@
+
+SOCKETCAN socketcan: uclibc $(TARGET_DIR)/usr/bin/$(SOCKETCAN_EXE)
+
+socketcan-source: $(DL_DIR)/$(SOCKETCAN_SOURCE)
+
+socketcan-clean:
+	$(MAKE) INSTALL_DIR=$(STAGING_DIR)/usr/bin -C $(SOCKETCAN_DIR) uninstall
+	-$(MAKE) -C $(SOCKETCAN_DIR) clean
+
+socketcan-dirclean:
+	rm -rf $(SOCKETCAN_DIR)
+
+
+############################################################
+#
+# Toplevel Makefile options
+#
+############################################################
+ifeq ($(strip $(BR2_PACKAGE_SOCKETCAN)),y)
+TARGETS+=socketcan
+endif
+
