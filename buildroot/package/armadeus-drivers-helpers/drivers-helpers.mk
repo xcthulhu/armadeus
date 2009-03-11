ed32c21e1eaafbab8a2cf8cc856954b90238f3fa
artemys <julien.boibessot@armadeus.com>
2009-03-11 14:53:27 +0000

    [BUILDROOT] Add a (fake) package to buildroot in order to install scripts useful for our custom drivers (for the moment only loadgpio.sh/gpio is concerned)
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@1146 556a79b9-cb07-0410-aa63-ea23e1810fdd

---
+++ buildroot/package/armadeus-drivers-helpers/drivers-helpers.mk
@@ -0,0 +1,28 @@
+#############################################################
+#
+# Armadeus Helpers script for loading Linux drivers
+#
+#############################################################
+
+ARMADEUS-DRIVERS_SOURCE_DIR:=$(BUILD_DIR)/../../target/linux/modules
+ARMADEUS-DRIVERS-HELPERS_TARGET_DIR:=$(TARGET_DIR)/usr/bin
+
+
+$(ARMADEUS-DRIVERS-HELPERS_TARGET_DIR)/loadgpio.sh: 
+	cp $(ARMADEUS-DRIVERS_SOURCE_DIR)/gpio/loadgpio.sh $(ARMADEUS-DRIVERS-HELPERS_TARGET_DIR)/
+#	find $(ARMADEUS-TESTSUITE_DIR) -name "*.sh" -exec install -D {} $(TARGET_DIR)/$(ARMADEUS-TESTSUITE_TARGET_DIR) \;
+
+armadeus-drivers-helpers: $(ARMADEUS-DRIVERS-HELPERS_TARGET_DIR)/loadgpio.sh
+
+armadeus-drivers-helpers-clean:
+	rm -f $(ARMADEUS-DRIVERS-HELPERS_TARGET_DIR)/loadgpio.sh
+
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+# Always installed by default
+TARGETS+=armadeus-drivers-helpers
+
