f1e7a490cc0af2a358dafaf532ec79ad71a91ad3
artemys <julien.boibessot@armadeus.com>
2009-03-27 22:19:31 +0000

    [DEMOS] Cleanup Xenomai demos
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@1193 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- target/demos/real_time/interrupt/linux/kernel/interrupt_linux_kernel.c
+++ target/demos/real_time/interrupt/linux/kernel/interrupt_linux_kernel.c
@@ -22,12 +22,11 @@
 */
 
 #include <linux/module.h>
-#include <asm/irq.h>
 #include <linux/irq.h>
-#include <asm/signal.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <asm/gpio.h>
+
 
 #define IRQ_NB IRQ_GPIOA(6)
 
@@ -36,45 +35,34 @@ MODULE_DESCRIPTION("irq kernel test");
 MODULE_SUPPORTED_DEVICE("none");
 MODULE_LICENSE("GPL");
 
+
 /* irq handler */
-irqreturn_t interrupt_handler (int irqn, void *dev){
+static irqreturn_t test_interrupt_handler(int irqn, void *dev)
+{
 	printk("IT !\n");
+
 	return IRQ_HANDLED;
 }
 
-/* loading (insmod) */
-static int __init irq_init(void) {
+static int __init test_irq_init(void)
+{
 	int err;
-	printk("irq init\n");
-	err =request_irq(IRQ_NB, interrupt_handler, 0,"test irq", NULL);
-	if (err!=0){
-		printk(KERN_INFO "request_irq : error ");
-		switch (err) {
-			case -EINVAL:
-				printk("EINVAL\n");
-				break;
-			case -ENOMEM:
-				printk("ENOMEM\n");
-				break;
-			case -EBUSY:
-				printk("EBUSY\n");
-				break;
-			case -ENXIO:
-				printk("ENXIO\n");
-				break;
-		}
+
+	err = request_irq(IRQ_NB, test_interrupt_handler, 0, "test irq", NULL);
+	if (err) {
+		printk(KERN_INFO "Error while requesting IRQ %d\n", IRQ_NB);
 		return err;
 	}
-	set_irq_type( IRQ_NB, IRQF_TRIGGER_FALLING ); 
+	set_irq_type(IRQ_NB, IRQF_TRIGGER_FALLING); 
+
 	return 0;
 }
 
-/* unloading (rmmod) */
-void __exit irq_exit(void) {
-	printk(KERN_INFO "irq_exit\n");
-	free_irq(IRQ_NB,NULL);
+static void __exit test_irq_exit(void)
+{
+	free_irq(IRQ_NB, NULL);
 }
 
-/* Api Linux devices */
-module_init(irq_init);
-module_exit(irq_exit);
+module_init(test_irq_init);
+module_exit(test_irq_exit);
+
