c10eb0d624456f7e53266da9d5792c86b9b71ed8
artemys <julien.boibessot@armadeus.com>
2009-05-07 15:12:28 +0000

    [TEST] Updates ADC test for APF27 & fast mode
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@1264 556a79b9-cb07-0410-aa63-ea23e1810fdd

--- target/test/test_adc.sh
+++ target/test/test_adc.sh
@@ -15,21 +15,46 @@ source ./test_helpers.sh
 source ./test_env.sh
 
 
+test_setup_apf9328()
+{
+	echo "I hope you didn't forget to wire EOC and CVNST !"
+	ADC_SPI_SYS="/sys/bus/spi/devices/spi1.0"
+}
+
+test_setup_apf27()
+{
+	ADC_SPI_SYS="/sys/bus/spi/devices/spi0.0"
+}
+
 test_ADC()
 {
 	show_test_banner "ADC"
 
 	modprobe max1027
 	if [ "$?" == 0 ]; then
+		execute_for_target test_setup_apf9328 test_setup_apf27
 		# Slow mode
-		let set=0x62; echo $set > /sys/bus/spi/devices/spi1.0/setup
+		let set=0x62; echo $set > $ADC_SPI_SYS/setup
+		let conv=0xf9; echo $conv > $ADC_SPI_SYS/conversion
 		echo "WAIT" > /dev/null
-		let conv=0xf9; echo $conv > /sys/bus/spi/devices/spi1.0/conversion
-		echo "WAIT" > /dev/null
-		temp=`cat /sys/bus/spi/devices/spi1.0/temp1_input`
-		if [ "$temp" != "0" ]; then
+		temp=`cat $ADC_SPI_SYS/temp1_input`
+		AIN0=`cat $ADC_SPI_SYS/in0_input`
+		if [ "$temp" != "0" ] && [ "$AIN0" != "0" ]; then
 			echo "Temp: $temp m°C"
-			echo_test_ok
+			echo "AIN0: $AIN0"
+		else
+			exit_failed
+		fi
+		# Fast mode
+		loadmax.sh
+		let set=0xb1; echo $set > $ADC_SPI_SYS/conversion
+		sleep 1
+		let set=0x48; echo $set > $ADC_SPI_SYS/setup
+		let set=0x20; echo $set > $ADC_SPI_SYS/averaging
+		echo "Reading in fast mode:"
+		dd if=/dev/max1027/AIN0 bs=2 count=10 | hexdump
+		if [ "$?" == 0 ]; then
+			echo_test_ok	
 		else
 			exit_failed
 		fi
