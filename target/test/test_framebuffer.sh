7e2bcfa1510f76d9c38dce4c61d67d9160fe33dd
artemys <julien.boibessot@armadeus.com>
2009-05-22 10:37:45 +0000

    [TEST] Add script to test framebuffer
    git-svn-id: https://armadeus.svn.sourceforge.net/svnroot/armadeus/trunk@1295 556a79b9-cb07-0410-aa63-ea23e1810fdd

---
+++ target/test/test_framebuffer.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#
+# Script to test Armadeus Software release
+#
+#  Copyright (C) 2008 The Armadeus Project
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+
+source ./test_helpers.sh
+source ./test_env.sh
+
+EXEC_NAME="fbtest"
+
+test_framebuffer()
+{
+	show_test_banner "Framebuffer"
+	echo 0 > /sys/class/graphics/fb0/blank   # Leave FB sleep mode
+
+	is_package_installed $EXEC_NAME
+	$EXEC_NAME
+	if [ "$?" == 0 ]; then
+		ask_user "Did all the tests show fine ? If OK say y"
+			if [ "$response" == "y" ]; then
+				echo_test_ok
+				exit 0
+			fi
+	fi
+	exit_failed
+}
+
+test_framebuffer
