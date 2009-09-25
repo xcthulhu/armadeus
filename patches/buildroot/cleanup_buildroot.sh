c61c10b22927cc0bb7ca2b03d57d1d25feef0979
Julien Boibessot <julien.boibessot@armadeus.com>
2009-09-25 14:14:24 +0200

    [BUILDROOT] Move GPM into a patch that can be sent to buildroot team (when it will work ;-))

--- patches/buildroot/cleanup_buildroot.sh
+++ patches/buildroot/cleanup_buildroot.sh
@@ -6,4 +6,5 @@
 # Removes packages added through patches:
 rm -rf buildroot/package/games/quake-pak/
 rm -rf buildroot/package/games/sdlquake/
+rm -rf buildroot/package/gpm/
 rm -rf buildroot/package/pygame/
