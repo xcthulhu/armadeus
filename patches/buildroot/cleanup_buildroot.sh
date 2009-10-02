8d4c5cf75905fbd00333b904624bb391cc8246e0
Julien Boibessot <julien.boibessot@armadeus.com>
2009-10-02 09:25:16 +0200

    updates patches/buildroot/cleanup_buildroot.sh

--- patches/buildroot/cleanup_buildroot.sh
+++ patches/buildroot/cleanup_buildroot.sh
@@ -8,3 +8,4 @@ rm -rf buildroot/package/games/quake-pak/
 rm -rf buildroot/package/games/sdlquake/
 rm -rf buildroot/package/gpm/
 rm -rf buildroot/package/pygame/
+rm -f buildroot/package/proftpd/proftpd-1.3.2-fix-kernel-header-capability-version.patch
