88bc49fe0f3651040c5fe87b663db3aee8c95aca
Fabien Marteau <fabien.marteau@armadeus.com>
2009-12-03 14:47:08 +0100

    [OTHER] spaces in AsDevices package

--- target/packages/as_devices/c/as_93lcxx.c
+++ target/packages/as_devices/c/as_93lcxx.c
@@ -46,18 +46,18 @@
 /** @brief Private functions to ease making spi instructions
  */
 
-uint8_t address_len (uint8_t aType, uint8_t aWord_size )
+uint8_t address_len(uint8_t aType, uint8_t aWord_size)
 {
-    switch (aType )
+    switch(aType)
     {
         case 46:
-            if (aWord_size == 16 )
+            if (aWord_size == 16)
                 return 6;
             else
                 return 7;
         case 56:
         case 66:
-            if (aWord_size == 16 )
+            if (aWord_size == 16)
                 return 8;
             else
                 return 9;
@@ -66,27 +66,27 @@ uint8_t address_len (uint8_t aType, uint8_t aWord_size )
     }
 }
 
-struct as_93lcxx_device * as_93lcxx_open (unsigned char *aSpidev_filename,
+struct as_93lcxx_device * as_93lcxx_open(unsigned char *aSpidev_filename,
                                           uint8_t aType,
                                           uint32_t aSpeed,
-                                          uint8_t aWord_size )
+                                          uint8_t aWord_size)
 {
     int fd=-1;
     struct as_93lcxx_device *dev;
 
     /* open spidev bus */
-    fd = as_spi_open (aSpidev_filename );
-    if (fd < 0 )
+    fd = as_spi_open(aSpidev_filename);
+    if (fd < 0)
         return NULL;
     
     /* verify datas */
-    if (! (  (aType == 46 ) ||  (aType == 56 ) ||  (aType == 66 ) ) )
+    if (!( (aType == 46) || (aType == 56) || (aType == 66)))
         return NULL;
-    if (  (aWord_size < 1 ) ||  (aWord_size > 16 ) )
+    if ( (aWord_size < 1) || (aWord_size > 16))
         return NULL;
 
-    dev =  (struct as_93lcxx_device *  )malloc (sizeof (struct as_93lcxx_device  )  );
-    if (dev == NULL )
+    dev = (struct as_93lcxx_device *)malloc(sizeof(struct as_93lcxx_device));
+    if (dev == NULL)
         return NULL;
 
     /* fill in spidev structure */
@@ -99,157 +99,157 @@ struct as_93lcxx_device * as_93lcxx_open (unsigned char *aSpidev_filename,
     return dev;
 }
 
-void as_93lcxx_close (struct as_93lcxx_device *aDev )
+void as_93lcxx_close(struct as_93lcxx_device *aDev)
 {
-    as_spi_close (aDev->fd );
-    free (aDev );
+    as_spi_close(aDev->fd);
+    free(aDev);
 }
 
-int32_t as_93lcxx_read (struct as_93lcxx_device *aDev, uint16_t aAddress )
+int32_t as_93lcxx_read(struct as_93lcxx_device *aDev, uint16_t aAddress)
 {
     uint32_t data_out,msg=0;
     int add_length;
 
-    add_length = address_len (aDev->type,aDev->word_size );
+    add_length = address_len(aDev->type,aDev->word_size);
 
     /* forge message */
-    msg =  ( (READ <<  (add_length+  (aDev->word_size ) ) )
-            |  (aAddress <<  (aDev->word_size ) ) );
+    msg = ((READ << (add_length+ (aDev->word_size)))
+            | (aAddress << (aDev->word_size)));
 
-    data_out = as_spi_msg (aDev->fd, msg,
+    data_out = as_spi_msg(aDev->fd, msg,
                           3 + add_length + aDev->word_size,
-                          aDev->speed );
+                          aDev->speed);
 
-    if (aDev->word_size == 8 )
+    if (aDev->word_size == 8)
         return data_out & 0xff;
     else
         return data_out & 0xffff;
 }
 
-int32_t as_93lcxx_ewen (struct as_93lcxx_device *aDev )
+int32_t as_93lcxx_ewen(struct as_93lcxx_device *aDev)
 {
     uint32_t msg=0;
     int add_length;
 
-    add_length = address_len (aDev->type,aDev->word_size );
+    add_length = address_len(aDev->type,aDev->word_size);
 
     /* forge message */
-    msg =  ( (EWEN << add_length )
-          | (EWEN_ADDR <<  (add_length-2 ) )
-             );
+    msg = ((EWEN << add_length)
+          |(EWEN_ADDR << (add_length-2))
+           );
 
-    as_spi_msg (aDev->fd, msg,
+    as_spi_msg(aDev->fd, msg,
                3 + add_length,
-               aDev->speed );
+               aDev->speed);
     return 0;
 }
 
-int32_t as_93lcxx_erase (struct as_93lcxx_device *aDev, uint16_t aAddress )
+int32_t as_93lcxx_erase(struct as_93lcxx_device *aDev, uint16_t aAddress)
 {
     uint32_t msg=0;
     int add_length;
 
-    add_length = address_len (aDev->type,aDev->word_size );
+    add_length = address_len(aDev->type,aDev->word_size);
 
     /* forge message */
-    msg =  ( (ERASE <<  (add_length + aDev->word_size  ) )
-            |  (aAddress <<  (aDev->word_size ) ) );
+    msg = ((ERASE << (add_length + aDev->word_size))
+            | (aAddress << (aDev->word_size)));
 
-    as_spi_msg (aDev->fd, msg,
+    as_spi_msg(aDev->fd, msg,
                3 + add_length + aDev->word_size,
-               aDev->speed );
+               aDev->speed);
 
-    usleep (TWC );
+    usleep(TWC);
 
     return 0;
 }
 
-int32_t as_93lcxx_erase_all (struct as_93lcxx_device *aDev )
+int32_t as_93lcxx_erase_all(struct as_93lcxx_device *aDev)
 {
     uint32_t msg=0;
     int add_length;
 
-    add_length = address_len (aDev->type,aDev->word_size );
+    add_length = address_len(aDev->type,aDev->word_size);
 
     /* forge message */
-    msg =  ( (ERAL << add_length )
-          | (ERAL_ADDR <<  (add_length-2 ) )
-             );
+    msg = ((ERAL << add_length)
+          |(ERAL_ADDR << (add_length-2))
+           );
 
-    as_spi_msg (aDev->fd, msg,
+    as_spi_msg(aDev->fd, msg,
                3 + add_length,
-               aDev->speed );
+               aDev->speed);
 
     /* wait for end of erase all */
-    usleep (TEC );
+    usleep(TEC);
 
     return 0;
 }
 
-int32_t as_93lcxx_write (struct as_93lcxx_device *aDev, 
-                         uint16_t aAddress, 
-                         uint16_t aValue )
+int32_t as_93lcxx_write(struct as_93lcxx_device *aDev, 
+                        uint16_t aAddress, 
+                        uint16_t aValue)
 {
     uint32_t msg=0;
     int add_length;
 
-    add_length = address_len (aDev->type,aDev->word_size );
+    add_length = address_len(aDev->type,aDev->word_size);
 
     /* forge message */
-    msg =  ( (WRITE <<  (add_length+  (aDev->word_size ) ) )
-            |  (aAddress <<  (aDev->word_size ) )
+    msg = ((WRITE << (add_length+ (aDev->word_size)))
+            | (aAddress << (aDev->word_size))
             | aValue
-             );
+           );
 
-    as_spi_msg (aDev->fd, msg,
+    as_spi_msg(aDev->fd, msg,
                3 + add_length + aDev->word_size,
-               aDev->speed );
+               aDev->speed);
 
     /* Wait for end of writing */
-    usleep (TWC );
+    usleep(TWC);
 
     return 0;
 }
 
-int32_t as_93lcxx_write_all (struct as_93lcxx_device *aDev, 
-                             uint16_t aValue )
+int32_t as_93lcxx_write_all(struct as_93lcxx_device *aDev, 
+                            uint16_t aValue)
 {
     uint32_t msg=0;
     int add_length;
 
-    add_length = address_len (aDev->type,aDev->word_size );
+    add_length = address_len(aDev->type,aDev->word_size);
 
     /* forge message */
-    msg =  ( (WRAL <<  (add_length+  (aDev->word_size ) ) )
-          | (WRAL_ADDR <<  ( (aDev->word_size ) +  (add_length-2 ) ) )
+    msg = ((WRAL << (add_length+ (aDev->word_size)))
+          |(WRAL_ADDR << ((aDev->word_size) + (add_length-2)))
           |aValue
-             );
+           );
 
-    as_spi_msg (aDev->fd, msg,
+    as_spi_msg(aDev->fd, msg,
                3 + add_length + aDev->word_size,
-               aDev->speed );
+               aDev->speed);
 
     /* Wait for end of write all */
-    usleep (TWL );
+    usleep(TWL);
 
     return 0;
 }
 
-int32_t as_93lcxx_ewds (struct as_93lcxx_device *aDev )
+int32_t as_93lcxx_ewds(struct as_93lcxx_device *aDev)
 {
     uint32_t msg=0;
     int add_length;
 
-    add_length = address_len (aDev->type,aDev->word_size );
+    add_length = address_len(aDev->type,aDev->word_size);
 
     /* forge message */
-    msg =  ( (EWDS << add_length )
-          | (EWDS_ADDR <<  (add_length-2 ) )
-             );
+    msg = ((EWDS << add_length)
+          |(EWDS_ADDR << (add_length-2))
+           );
 
-    as_spi_msg (aDev->fd, msg,
+    as_spi_msg(aDev->fd, msg,
                3 + add_length,
-               aDev->speed );
+               aDev->speed);
     return 0;
 }
 
--- target/packages/as_devices/c/as_spi.h
+++ target/packages/as_devices/c/as_spi.h
@@ -2,7 +2,7 @@
  * This software is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of the
- * License, or  (at your option) any later version.
+ * License, or (at your option) any later version.
  * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,7 +14,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  *
- * Copyright  (C) 2009 Fabien Marteau <fabien.marteau@armadeus.com> 
+ * Copyright (C) 2009 Fabien Marteau <fabien.marteau@armadeus.com> 
  *
  */
 
@@ -38,7 +38,7 @@ extern "C" {
  *
  * @return the file descriptor of the opened device, -1 on error
  */
-int as_spi_open (const unsigned char *spidev_name);
+int as_spi_open(const unsigned char *spidev_name);
 
 /** @brief Set spi bus mode
  *
@@ -48,7 +48,7 @@ int as_spi_open (const unsigned char *spidev_name);
  * @return mode if positive value, negative value on error
  *
  */
-int as_spi_set_mode (int fd, uint8_t mode);
+int as_spi_set_mode(int fd, uint8_t mode);
 
 /** @brief Set bits order
  *
@@ -57,7 +57,7 @@ int as_spi_set_mode (int fd, uint8_t mode);
  *
  * @return lsb first if positive, msb first if 0, negative value on error
  */
-int as_spi_set_lsb (int fd, uint8_t lsb);
+int as_spi_set_lsb(int fd, uint8_t lsb);
 
 /** @brief Set bits per word
  *
@@ -66,7 +66,7 @@ int as_spi_set_lsb (int fd, uint8_t lsb);
  *
  * @return bit per word if positive value, negative value on error
  */
-int as_spi_set_bits_per_word (int fd, uint8_t bits);
+int as_spi_set_bits_per_word(int fd, uint8_t bits);
 
 /** @brief Get spi bus mode
  *
@@ -75,7 +75,7 @@ int as_spi_set_bits_per_word (int fd, uint8_t bits);
  * @return mode if positive value, negative value on error
  *
  */
-int as_spi_get_mode (int fd);
+int as_spi_get_mode(int fd);
 
 /** @brief Get bits order
  *
@@ -83,7 +83,7 @@ int as_spi_get_mode (int fd);
  *
  * @return lsb first if positive, msb first if 0, negative value on error
  */
-int as_spi_get_lsb (int fd);
+int as_spi_get_lsb(int fd);
 
 /** @brief Get clock bus speed
  *
@@ -91,7 +91,7 @@ int as_spi_get_lsb (int fd);
  *
  * @return speed in Hz, negative value on error
  */
-int as_spi_get_speed (int fd);
+int as_spi_get_speed(int fd);
 
 
 /** @brief Get bits per word
@@ -100,9 +100,9 @@ int as_spi_get_speed (int fd);
  *
  * @return bit per word if positive value, negative value on error
  */
-int as_spi_get_bits_per_word (int fd);
+int as_spi_get_bits_per_word(int fd);
 
-/** @brief Forge arbitrary length message  (31bits max) and send it
+/** @brief Forge arbitrary length message (31bits max) and send it
  *
  * @param fd spidev file handler
  * @param msg right adjusted message
@@ -111,15 +111,15 @@ int as_spi_get_bits_per_word (int fd);
  *
  * @return message read
  */
-uint32_t as_spi_msg (int fd, 
-                     uint32_t msg, 
-                     size_t len,
-                     uint32_t speed);
+uint32_t as_spi_msg(int fd, 
+                    uint32_t msg, 
+                    size_t len,
+                    uint32_t speed);
 
 /** @brief Close a SPI bus.
  *
  */
-void as_spi_close (int fd);
+void as_spi_close(int fd);
 
 
 #ifdef __cplusplus
