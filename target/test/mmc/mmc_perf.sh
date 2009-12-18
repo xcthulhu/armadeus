b7fb09812f5fe6bb7f86face8ab3eb80dd38a4e6
Julien Boibessot <julien.boibessot@armadeus.com>
2009-12-18 16:41:54 +0100

    [TEST] Makes mmc_perf.sh usable with USB keys

--- target/test/mmc/mmc_perf.sh
+++ target/test/mmc/mmc_perf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #
-# Script to test Armadeus boards performances to handle MMC/SD cards
+# Script to test Armadeus boards performances to handle removable storage media
 #
 #  Copyright (C) 2007 The Armadeus Project
 #
@@ -23,11 +23,15 @@
 source ./test_helpers.sh
 
 # Script parameters
+TEMP_FILE_SIZE=16384 # kbytes (ex: 16384 24576)
+NB_ITER=10
+
 MMC_MOUNT_DIR="/media/mmc"
 MMC_DEVICE="/dev/mmcblk0p1"
+USB_DEVICE="/dev/sda1"
+USB_MOUNT_DIR="/media/usbdisk"
 TEMP_DIR="/tmp/perf_mmc/"
 TEMP_FILE="/tmp/data.bin"
-TEMP_FILE_SIZE=16384 # kbytes
 WRITE_BENCH="/tmp/test_mmc.sh"
 
 WTIME=0
@@ -43,23 +47,24 @@ debug()
 
 get_time_in_ms_from_file()
 {
-    if [ -f "$1" ]; then
-        TIME=`cat $1 | grep real | cut -d m -f 2 | sed 's/s//'`
-        STIME=`echo $TIME | cut -d . -f 1`
-        STIME=`expr $STIME \* 1000`
-        MSTIME=`echo $TIME | cut -d . -f 2`
-        MSTIME=`expr $MSTIME \* 10`
-        NAME=`echo $1 | grep write`
-        if [ "$NAME" != "" ]; then
-            WTIME=`expr $STIME + $MSTIME`
-            debug "WTIME: $WTIME"
-        else
-            RTIME=`expr $STIME + $MSTIME`
-            debug "RTIME: $RTIME"
-        fi
-    else
-        echo "File doesn't exist"
-    fi
+	if [ ! -f "$1" ]; then
+		echo "File doesn't exist"
+		return 1
+	fi
+
+	TIME=`cat $1 | grep real | cut -d m -f 2 | sed 's/s//'`
+	STIME=`echo $TIME | cut -d . -f 1`
+	STIME=`expr $STIME \* 1000`
+	MSTIME=`echo $TIME | cut -d . -f 2`
+	MSTIME=`expr $MSTIME \* 10`
+	NAME=`echo $1 | grep write`
+	if [ "$NAME" != "" ]; then
+		WTIME=`expr $STIME + $MSTIME`
+		debug "WTIME: $WTIME"
+	else
+		RTIME=`expr $STIME + $MSTIME`
+		debug "RTIME: $RTIME"
+	fi
 }
 
 test_speed()
@@ -67,41 +72,43 @@ test_speed()
 	# Create temp bench file
 	create_random_file $TEMP_FILE $TEMP_FILE_SIZE
 
-    # Create write bench script
-    echo "mv $TEMP_FILE $MMC_MOUNT_DIR" > $WRITE_BENCH
-    echo "sync" >> $WRITE_BENCH
-    echo "exit 0" >> $WRITE_BENCH
+	# Create write bench script
+	echo "mv $TEMP_FILE $MOUNT_DIR" > $WRITE_BENCH
+	echo "sync" >> $WRITE_BENCH
+	echo "exit 0" >> $WRITE_BENCH
 
-    for it in 1 2 3 4 5; do
-        echo "--- Test iteration n°$it ---"
-        echo "  Writing"
-        time sh $WRITE_BENCH 2>/tmp/writetime
-        umount $MMC_MOUNT_DIR && mount $MMC_DEVICE $MMC_MOUNT_DIR
-        echo "  Reading"
-        time mv $MMC_MOUNT_DIR/data.bin /tmp/ 2>/tmp/readtime
-        # Update writing mean time
-        get_time_in_ms_from_file /tmp/writetime
-        if [ $it == "1" ]; then
-            WTOTIME=$WTIME
-        else
-            WTOTIME=`expr $WTIME + $WTOTIME`
-        fi
-        debug "Total write time: $WTOTIME ms"
-        # Update reading mean time
-        get_time_in_ms_from_file /tmp/readtime
-        if [ $it == "1" ]; then
-            RTOTIME=$RTIME
-        else
-            RTOTIME=`expr $RTIME + $RTOTIME`
-        fi
-        debug "Total read time: $RTOTIME ms"
-    done
+	for it in `seq $NB_ITER`; do
+		echo "--- Test iteration n°$it ---"
+		echo "  Writing"
+		time sh $WRITE_BENCH 2>/tmp/writetime
+		sync
+		umount $MOUNT_DIR && mount $REM_DEVICE $MOUNT_DIR
+		echo "  Reading"
+		time mv $MOUNT_DIR/data.bin /tmp/ 2>/tmp/readtime
+		# Update writing mean time
+		get_time_in_ms_from_file /tmp/writetime
+		if [ $it == "1" ]; then
+			WTOTIME=$WTIME
+		else
+			WTOTIME=`expr $WTIME + $WTOTIME`
+		fi
+		debug "Total write time: $WTOTIME ms"
+		# Update reading mean time
+		get_time_in_ms_from_file /tmp/readtime
+		if [ $it == "1" ]; then
+			RTOTIME=$RTIME
+		else
+			RTOTIME=`expr $RTIME + $RTOTIME`
+		fi
+		debug "Total read time: $RTOTIME ms"
+	done
 
 	WTOTIME=`expr $WTOTIME / $it`
 	RTOTIME=`expr $RTOTIME / $it`
 	WSPEED=`expr $TEMP_FILE_SIZE \* 1000 / $WTOTIME`
 	RSPEED=`expr $TEMP_FILE_SIZE \* 1000 / $RTOTIME`
 	echo "--- Test result (mean values): read -> $RSPEED kBytes/sec   write -> $WSPEED kBytes/sec"
+	umount $MOUNT_DIR
 }
 
 usage()
@@ -115,11 +122,6 @@ usage()
 # Main
 #
 
-# Check some pre-requisites
-if [ ! -d $MMC_MOUNT_DIR ]; then
-	echo "Check that $MMC_MOUNT_DIR directory is existing"
-	exit 1
-fi
 # Process parameters
 if [ "$#" == 0 ]; then
 	usage
@@ -131,22 +133,47 @@ if [ "$2" != "" ]; then
 	MMC_DEVICE="$2"
 fi
 
-if [ ! -b $MMC_DEVICE ]; then
+if [ ! -b $MMC_DEVICE ] && [ ! -b $USB_DEVICE ]; then
 	echo -n "Please insert your MMC/SD/microSD/USB key"
 	it=0
-	while [ ! -b $MMC_DEVICE ] && [ $it -le 20 ]; do
+	while [ ! -b $MMC_DEVICE ] && [ ! -b $USB_DEVICE ] && [ $it -le 20 ]; do
 		it=$((it+1))
 		sleep 2
 		echo -n "."
 	done
 fi
 
+if [ -b $MMC_DEVICE ]; then
+	echo "Testing MMC/SD/microSD"
+	REM_DEVICE=$MMC_DEVICE
+	MOUNT_DIR=$MMC_MOUNT_DIR
+elif [ -b $USB_DEVICE ]; then
+	echo "Testing USB key"
+	REM_DEVICE=$USB_DEVICE
+	MOUNT_DIR=$USB_MOUNT_DIR
+else
+	echo "No removable device found: exiting..."
+	exit 1
+fi
+
+# Check some pre-requisites
+if [ ! -d $MOUNT_DIR ]; then
+	echo "Create $MOUNT_DIR directory ? (y/N)"
+	read answer
+	if [ "$answer" == "y" ] || [ "$answer" == "yes" ]; then
+		mkdir -p $MOUNT_DIR
+	else
+		"Exiting..."
+		exit 1
+	fi
+fi
+
 # If not mounted, mount it:
-IS_MOUNTED=`mount | grep -c $MMC_MOUNT_DIR`
+IS_MOUNTED=`mount | grep -c $MOUNT_DIR`
 if [ "$IS_MOUNTED" != "1" ]; then
-	mount $MMC_DEVICE $MMC_MOUNT_DIR
+	mount $REM_DEVICE $MOUNT_DIR
 	if [ "$?" != 0 ]; then
-		echo "Failed to mount device $MMC_DEVICE"
+		echo "Failed to mount device $REM_DEVICE"
 		exit 1
 	fi
 fi
@@ -172,7 +199,7 @@ dd if=/dev/urandom of=$FILE bs=1024 count=1024 2>/dev/null
 md5sum $FILE > $MD5
 
 while [ $it -le $SIZE ]; do
-	FILEMMC=$MMC_MOUNT_DIR/test_$it.rnd
+	FILEMMC=$MOUNT_DIR/test_$it.rnd
 #	MD5=$TEMP_DIR/test_$it.rnd.md5
 	echo -en "\r Copying: $FILEMMC"
 	it=$((it+1))
@@ -182,15 +209,15 @@ done
 
 echo ""
 
-umount $MMC_MOUNT_DIR
-mount $MMC_DEVICE $MMC_MOUNT_DIR
+umount $MOUNT_DIR
+mount $REM_DEVICE $MOUNT_DIR
 
 date
-FILES=`ls $MMC_MOUNT_DIR/*.rnd`
+FILES=`ls $MOUNT_DIR/*.rnd`
 for file in $FILES; do
 	file=`basename $file`
 	echo -en "\r Checking MD5 for: $file"
-	MD5MMC=`md5sum $MMC_MOUNT_DIR/$file | cut -d " " -f 1`
+	MD5MMC=`md5sum $MOUNT_DIR/$file | cut -d " " -f 1`
 	MD5ORG=`cat $MD5 | cut -d " " -f 1`
 	if [ "$MD5MMC" != "$MD5ORG" ]; then
 		echo -e "\nMD5 failed for $file: $MD5MMC $MD5ORG"
@@ -202,6 +229,6 @@ date
 echo "Integrity test successful !"
 
 rm -rf $TEMP_DIR
-rm $MMC_MOUNT_DIR/*.rnd
+rm $MOUNT_DIR/*.rnd
 
 exit 0
