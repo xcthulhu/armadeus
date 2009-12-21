a68f8f37e142155515ad3e912d85fe79e800d853
Julien Boibessot <julien.boibessot@armadeus.com>
2009-12-21 13:47:12 +0100

    [TOOLS] Cleanup imxssi debug tool and add it Audio In -> Audio Out loop option

--- target/linux/debug/imxssi/imxssi.c
+++ target/linux/debug/imxssi/imxssi.c
@@ -31,68 +31,75 @@
 #include <fcntl.h>
 #include <ctype.h>
 #include <math.h>
+#include <getopt.h>
 
 
 //#define DEBUG TRUE
-
-#ifdef IMXL
-#include "imxregs.h"
-#define SSI_TRANSMIT_DATA_REG    0x00218000
-#define SSI_FIFO_STATUS_REGISTER 0x00218020
+#ifdef DEBUG
+# define debug(fmt, arg...) printf(fmt, ##arg)
 #else
-#include "imx27regs.h"
-#define SSI_TRANSMIT_DATA_REG    0x10010000
-#define SSI_FIFO_STATUS_REGISTER 0x1001002C
+# define debug(fmt, arg...) ({ if (0) printf(fmt, ##arg);})
 #endif
-#define TFCNT_MASK               0x0F00
+#ifdef IMXL
+# include "imxregs.h"
+# define SSI_TRANSMIT_DATA_REG    0x00218000
+# define SSI_FIFO_STATUS_REGISTER 0x00218020
+#else
+# include "imx27regs.h"
+# define SSI_TRANSMIT_DATA_REG    0x10010000	/* FIFO 0 */
+# define SSI_RECEIVE_DATA_REG     0x10010008
+# define SSI_FIFO_STATUS_REGISTER 0x1001002C
+#endif
+#define RFCNT_MASK               0xF000
+#define TFCNT_MASK               0x0F00		/* FIFO 0 for i.MX27 */
+#define RFCNT_SHIFT              12
 #define TFCNT_SHIFT              8
+#define TYPE_TRIANGLE            0
+#define TYPE_SINUS               1
+#define TYPE_LOOP                2
 
-
-// fd for /dev/mem
 static int fd = -1;
 #define TEST_SIGNAL_MAX_SAMPLES 130712
 static int *table;
 // static int ttable[130712];
 
-// Get value of the register at the given address
+
+/* Get value of the register at the given address */
 static int getmem(u32 addr)
 {
     void *map, *regaddr;
     u32 val;
-    #ifdef DEBUG
-    printf("getmem(0x%08x)\n", addr);
-    #endif // DEBUG
+
+    debug("getmem(0x%08x)\n", addr);
     if (fd == -1) {
         fd = open("/dev/mem", O_RDWR | O_SYNC);
-        if (fd<0) {
+        if (fd < 0) {
             perror("open(\"/dev/mem\")");
             exit(1);
         }
     }
     
-    map = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, addr & ~MAP_MASK );
-    if (map == (void*)-1 ) {
+    map = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, addr & ~MAP_MASK);
+    if (map == (void*)-1) {
         perror("mmap()");
         exit(1);
     }
-    #ifdef DEBUG
-    printf("Mapped addr: 0x%x\n", regaddr);
-    #endif // DEBUG
+    debug("Mapped addr: 0x%x\n", regaddr);
     regaddr = map + (addr & MAP_MASK);
     
     val = *(u32*) regaddr;
-    munmap(0,MAP_SIZE);
+    munmap(0, MAP_SIZE);
     
     return val;
 }
 
-// Modify register value at given address
+/* Modify register value at given address */
 static void putmem(u32 addr, u32 val)
 {
     void *map, *regaddr;
     static int fd = -1;
     
-    //printf("putmem(0x%08x, 0x%08x)\n", addr, val);
+    /* printf("putmem(0x%08x, 0x%08x)\n", addr, val); */
     
     if (fd == -1) {
         fd = open("/dev/mem", O_RDWR | O_SYNC);
@@ -102,20 +109,19 @@ static void putmem(u32 addr, u32 val)
         }
     }
     
-    map = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, addr & ~MAP_MASK );
-    if (map == (void*)-1 ) {
+    map = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, addr & ~MAP_MASK);
+    if (map == (void*)-1) {
         perror("mmap()");
         exit(1);
     }
-    printf("Mapped addr: 0x%x\n", regaddr);
+    debug("Mapped addr: 0x%x\n", regaddr);
     regaddr = map + (addr & MAP_MASK);
     
     *(u32*) regaddr = val;
-    munmap(0,MAP_SIZE);
+    munmap(0, MAP_SIZE);
 }
 
-//
-// Set content of register with given name
+/* Set content of register with given name */
 static void setreg(char *name, u32 val)
 {
     int i;
@@ -125,35 +131,31 @@ static void setreg(char *name, u32 val)
     int n=sizeof(regs)/sizeof(struct reg_info);
     
     for (i=0; i<n; i++) {
-        if (strcmp(regs[i].name, name)==0) {
+        if (strcmp(regs[i].name, name) == 0) {
             found = i;
-            //printf("Matched %s with %s, count=%d\n", regs[i].name, name, count);
             count++;
         }
     }
-    if (count!=1) {
+    if (count != 1) {
         printf("No or more than one matching register found\n");
         exit(1);
     }
     
     mem = getmem(regs[found].addr);
-    //printf("Old contents: 0x%08x\n", mem);
     mem &= ~(regs[found].mask << regs[found].shift);
-    //printf("Unmasked contents: 0x%08x\n", mem);
     val &= regs[found].mask;
-    //printf("mask: 0x%08x\n", regs[found].mask);
-    //printf("masked val: 0x%08x\n", val);
     mem |= val << regs[found].shift;
-    //printf("Embedded value: 0x%08x\n", mem);
     putmem(regs[found].addr, mem);
 }
 
 static void audio(u32 type, u32 samples, float level)
 {
-    volatile void *map, *regaddr, *dma;
+    volatile void *map, *STX0, *SRX0, *SFCSR, *dma;
     static int fd = -1;
     u32 counter = 0;
     u32 i;
+
+    debug("type %d samples %d level %f\n", type, samples, level);
 
     table = malloc(TEST_SIGNAL_MAX_SAMPLES*sizeof(*table));
     if (!table) {
@@ -162,49 +164,47 @@ static void audio(u32 type, u32 samples, float level)
     }
 
     if (samples > 65536) samples = 65536;
-    if (-1.0f > level) -1.0f;
-    if ( 1.0f < level) 1.0f;
+    if (level < -1.0f) level = -1.0f;
+    if (level > 1.0f) level = 1.0f;
     
-//     for (i=0;i<65536;i++)
-//         ttable[i] = 0;
+/*     for (i=0;i<65536;i++)
+         ttable[i] = 0; */
     
-    if (0 == (type%2)) {
-        u32 i=0;
+    if (type == TYPE_TRIANGLE) {
         for (i=0; i<(samples/2); i++) {
-            table[i] = roundf((-32768+ ((i*(65535/samples))*2*level)));
-            table[samples-1-i] = roundf(((-32768 + ((i*(65535/samples))*2*level))));
+            table[i] = roundf(-32768 + ((i*(65535/samples))*2*level));
+            table[samples-1-i] = roundf(-32768 + ((i*(65535/samples))*2*level));
 //              printf("tab[%d]=%x tab[%d]=%x\n", i, table[i], samples-1-i, table[samples-1-i]);
         }
-    } else {
-        u32 i=0;
+    } else if (type == TYPE_SINUS) {
         double cosx;
-        for (i=0;i<samples;i++) {
+        for (i=0; i<samples; i++) {
             cosx = cos(((double)i/samples)*2*M_PI);
             table[i] = roundf(cosx*32767*level);
         }
     }
     
-    //printf("putmem(0x%08x, 0x%08x)\n", SSI_TRANSMIT_DATA_REG, val);
-    
     if (fd == -1) {
         fd = open("/dev/mem", O_RDWR | O_SYNC);
-        if (fd<0) {
+        if (fd < 0) {
             perror("open(\"/dev/mem\")");
             exit(1);
         }
     }
     
-    map = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, SSI_TRANSMIT_DATA_REG & ~MAP_MASK );
-    if (map == (void*)-1 ) {
+    map = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, SSI_TRANSMIT_DATA_REG & ~MAP_MASK);
+    if (map == (void*)-1) {
         perror("mmap() ssi");
         exit(1);
     }
-    printf("Mapped ssi addr: 0x%x\n", map);
-    regaddr = map + (SSI_TRANSMIT_DATA_REG & MAP_MASK);
+    printf("Mapped SSI reg addr: 0x%x\n", map);
+    STX0 = map + (SSI_TRANSMIT_DATA_REG & MAP_MASK);
+    SRX0 = map + (SSI_RECEIVE_DATA_REG & MAP_MASK);
+    SFCSR = map + (SSI_FIFO_STATUS_REGISTER & MAP_MASK);
 
 #ifdef USE_DMA
-    dma = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x00209000 & ~MAP_MASK );
-    if (dma == (void*)-1 ) {
+    dma = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x00209000 & ~MAP_MASK);
+    if (dma == (void*)-1) {
         perror("mmap() dma");
         exit(1);
     }
@@ -212,7 +212,7 @@ static void audio(u32 type, u32 samples, float level)
     *(volatile u32*)(dma+0x2CC) = 0x20A0; /* CCR9 */
 #endif
 
-    if (type > 1) {
+    if (type > 2) {
 #ifdef USE_DMA
         *(volatile u32*)(dma+0x2CC) = 0x2000; /* CCR9 */
         *(volatile u32*)(dma+0x2C0) = table; /* SAR9 */
@@ -223,12 +223,10 @@ static void audio(u32 type, u32 samples, float level)
         *(volatile u32*)(dma+0x2D4) = 16; /* BLR9 */
         *(volatile u32*)(dma+0x2CC) = 0x000F; /* CCR9 */
 #endif
-        while(1) {
-            while(6 < ((*(volatile u32*)(map + (SSI_FIFO_STATUS_REGISTER & MAP_MASK))) & TFCNT_MASK) >> TFCNT_SHIFT );
-            
+        while (1) {
+            while (6 < ((*(volatile u32*)(map + (SSI_FIFO_STATUS_REGISTER & MAP_MASK))) & TFCNT_MASK) >> TFCNT_SHIFT);
             {
-                u32 i;
-                for (i=0;i<samples;i++)
+                for (i=0; i<samples; i++)
                     printf("tab[%d]=%d tab[%d]=%d\n", i, table[i], i, /*t*/table[i]);
                 /**(volatile short*) regaddr = 32000;*/
                 /*	  *(volatile short*) regaddr = table[counter&0X0FFFF];
@@ -237,66 +235,127 @@ static void audio(u32 type, u32 samples, float level)
                 */
             }
         }
+#ifndef IMXL
+    } else if (type == TYPE_LOOP) {
+    /* Loop audio in to audio out (only possible on APF27Dev) */
+        while (1) {
+            /* Wait until Rx FIFO is half full */
+            while (((*(volatile u32*)(SFCSR) & RFCNT_MASK) >> RFCNT_SHIFT) < 4);
+            /* then copy Rx FIFO to Tx one */
+            for (i=0; i<4; i++)
+                *(volatile u32*) STX0 = *(volatile u32*) SRX0;
+        }
+#endif
     } else {
-        while(1) {
+    /* Ramp or sin ouput */
+        while (1) {
             /* Wait 2 empty spaces in Transmit FIFO */
-            while(6 < ((*(volatile u32*)(map + (SSI_FIFO_STATUS_REGISTER & MAP_MASK))) & TFCNT_MASK) >> TFCNT_SHIFT );
+            while (6 < ((*(volatile u32*)(SFCSR)) & TFCNT_MASK) >> TFCNT_SHIFT);
 
-            *(volatile u32*) regaddr = table[counter & 0x0FFFF];
-            *(volatile u32*) regaddr = table[counter & 0x0FFFF];
+            *(volatile u32*) STX0 = table[counter & 0x0FFFF];
+            *(volatile u32*) STX0 = table[counter & 0x0FFFF];
             counter = (counter+1) % samples;
         }
     }
 
     free(table);
-    munmap(0,MAP_SIZE);
+    munmap(0, MAP_SIZE);
 }
 
+static void usage(const char* prog_name)
+{
+    printf("Version: " VERSION "\n");
+    printf("Usage: %s [-t type] [-l level] [-s samples]\n\n"
+           "Options:\n"
+           "        -h, --help    this help\n"
+#ifndef IMXL
+           "        -L, --loop    loop Audio In to Audio Out\n"
+#endif
+           "        -t type       0:triangular/ramp  1:sinusoid\n"
+           "        -s samples    nb of samples per period\n"
+           "        -l level      0.0 .. 1.0 signal amplitude\n\n"
+           "    !! tsc210x has to be configured in master mode !!\n",
+        prog_name);
 
-/////////////////////////////////////////////////////////////////////////////////////
-// MAIN
-//
+    exit(1);
+}
+
 int main(int argc, char *argv[])
 {
     char *p;
     u32 val;
+    u32 type;
+    float level;
 
-    if (argc == 4) {
-        u32 type;
-        float level;
+    int option, option_index = 0;
+    static struct option long_options[] = {
+        {"help", 0, 0, 'h'},
+        {"loop", 0, 0, 'L'},
+        {0, 0, 0, 0}
+    };
 
-#ifdef IMXL
-        setreg("SFCSR", 0x0088);
-        setreg("STCCR", 0x6103);
-        setreg("SRCCR", 0x6103);
-        setreg("STCR",  0x028D);
-        setreg("SCSR",  0xDB41);
-#else
-        setreg("PCCR0",   0xd5010013);
-        setreg("PCCR1",   0xa21a0e30);
-        setreg("PPCR1",   0x00001000);
-        setreg("HPCR1",   0xcc007000);
-        setreg("STCR_1",  0x0000028d); /* I2S mode */
-        setreg("SRCR_1",  0x0000028d); /* I2S mode */
-        setreg("STCCR_1", 0x0000e000);
-        setreg("SCR_1",   0x00000057); /* I2S mode */
-#endif
+    /* default values : */
+    type = TYPE_TRIANGLE;
+    val = 200;
+    level = 0.2;
 
-        sscanf(argv[1],"%i",&type);
-        sscanf(argv[2],"%i",&val);
-        sscanf(argv[3],"%f",&level);
-        audio(type, val, level);
+    opterr = 0;
+    while ((option = getopt_long(argc, argv, "hLt:s:l:", long_options, &option_index)) != -1)
+    {
+        switch (option)
+        {
+            case 't':
+                type = atoi(optarg);
+            break;
 
-        return 0;
+            case 's':
+                val = atoi(optarg);
+            break;
+
+            case 'h':
+                usage(argv[0]);
+            break;
+
+            case 'l':
+                 level = atof(optarg);
+            break;
+
+            case 'L':
+                type = TYPE_LOOP;
+            break;
+
+            case '?':
+                if ((optopt == 's') || (optopt == 't') || (optopt == 'l'))
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint(optopt))
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+                usage(argv[0]);
+
+            default:
+                abort();
+        }
     }
-    // In all other cases, print Usage
-    printf("Version : " VERSION "\n");
-    printf("Usage: %s <signal_type> <samples> <scale> - to audio signal:\n"
-           "          <signal_type> 0:triangular 1:sinusoid\n"
-           "          <samples> nb sample per period\n"
-           "          <scale>  0.0 .. 1.0 signal amplitude\n"
-           "    !! tsc210x has to be configured in master mode !!\n",
-        argv[0], argv[0], argv[0], argv[0]);
+ 
+#ifdef IMXL
+    setreg("SFCSR", 0x0088);
+    setreg("STCCR", 0x6103);
+    setreg("SRCCR", 0x6103);
+    setreg("STCR", 0x028D);
+    setreg("SCSR", 0xDB41);
+#else
+    setreg("PCCR0", 0xd5010013);
+    setreg("PCCR1", 0xa21a0e30);
+    setreg("PPCR1", 0x00001000);
+    setreg("HPCR1", 0xcc007000);
+    setreg("STCR_1", 0x0000028d); /* I2S mode */
+    setreg("SRCR_1", 0x0000028d); /* I2S mode */
+    setreg("STCCR_1", 0x0000e000);
+    setreg("SCR_1", 0x00000057); /* I2S mode */
+#endif
+    audio(type, val, level);
 
-    return 1;
+    return 0;
 }
+
