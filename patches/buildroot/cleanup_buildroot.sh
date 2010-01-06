d444ccf4e79a868c4e69070139a55e9b0e4967f1
Julien Boibessot <julien.boibessot@armadeus.com>
2010-01-06 22:52:08 +0100

    [BUILDROOT] Add uucp package (preliminary) + updates cleanup_buildroot.sh

--- patches/buildroot/cleanup_buildroot.sh
+++ patches/buildroot/cleanup_buildroot.sh
@@ -3,9 +3,11 @@
 # Things to do before patching Buildroot
 #
 
-# Removes packages added through patches:
+# Removes packages/files added through patches:
 rm -rf buildroot/package/games/quake-pak/
 rm -rf buildroot/package/games/sdlquake/
 rm -rf buildroot/package/gpm/
 rm -rf buildroot/package/pygame/
 rm -f buildroot/package/proftpd/proftpd-1.3.2-fix-kernel-header-capability-version.patch
+rm -rf buildroot/package/uucp/
+rm -f buildroot/package/audio/libsndfile/libsndfile-1.0.*-srconly.patch
