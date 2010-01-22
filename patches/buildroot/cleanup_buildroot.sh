e7e0f350d66db3289bac08a99a244e55628472c7
Julien Boibessot <julien.boibessot@armadeus.com>
2010-01-22 17:03:24 +0100

    Fixes patches/buildroot/cleanup_buildroot.sh

--- patches/buildroot/cleanup_buildroot.sh
+++ patches/buildroot/cleanup_buildroot.sh
@@ -8,7 +8,7 @@ rm -rf buildroot/package/games/quake-pak/
 rm -rf buildroot/package/games/sdlquake/
 rm -rf buildroot/package/gpm/
 rm -rf buildroot/package/pygame/
-rm -f buildroot/package/proftpd/proftpd-1.3.2-fix-kernel-header-capability-version.patch
+rm -f  buildroot/package/proftpd/proftpd-1.3.2-fix-kernel-header-capability-version.patch
 rm -rf buildroot/package/uucp/
-rm -f buildroot/package/audio/libsndfile/libsndfile-1.0.*-srconly.patch
-rm -rf package/audio/flite/
+rm -f  buildroot/package/audio/libsndfile/libsndfile-1.0.*-srconly.patch
+rm -rf buildroot/package/audio/flite/
