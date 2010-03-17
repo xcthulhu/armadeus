e6e6e857d141e1fee87806fb6d72bd90f7d9759e
Julien Boibessot <julien.boibessot@armadeus.com>
2010-03-17 11:52:43 +0100

    [DEMOS] Cleanup in oscillo & real_time/fractal

--- target/demos/oscillo/lcd.c
+++ target/demos/oscillo/lcd.c
@@ -21,26 +21,16 @@
  *
  */
 
-
 #include <errno.h>
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <ctype.h>
-
 #include <sys/ioctl.h>
 #include <sys/time.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <fcntl.h>
 #include <linux/fb.h>
-#include <sys/mman.h>
 
 #include "lcd.h"
 
