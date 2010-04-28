9a8fafb0679025cb5c15f93339022d0c5504d7db
Julien Boibessot <julien.boibessot@armadeus.com>
2010-04-28 17:47:36 +0200

    [BUILDROOT] Add an infrastructure to install closed source linux-firmwares

--- patches/buildroot/cleanup_buildroot.sh
+++ patches/buildroot/cleanup_buildroot.sh
@@ -16,3 +16,4 @@ rm -rf buildroot/package/libcurl/
 rm -f  buildroot/package/audio/madplay/madplay-audio-alsa.patch
 rm -f  buildroot/package/mplayer/mplayer-1.0rc1-add_pld_asm_inst_for_non_armv5_arch.patch
 rm -f  buildroot/package/busybox/busybox-1.12.1-speed_table-B921600.patch
+rm -rf buildroot/package/firmware/
