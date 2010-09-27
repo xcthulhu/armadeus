2da68cd6dc7eeeb31b7394f75891ab0177ac6397
Julien Boibessot <julien.boibessot@armadeus.com>
2010-09-27 09:56:56 +0200

    Indentation

--- target/packages/as_devices/c/as_i2c.c
+++ target/packages/as_devices/c/as_i2c.c
@@ -30,7 +30,7 @@
 #include "as_i2c.h"
 
 
-/// Format of path to I2C devices
+/* Format of path to I2C devices */
 #define AS_I2C_DEV_PATH_FMT "/dev/i2c-%u"
 #define _STR(x) #x
 #define AS_I2C_DEV_PATH_SIZE (sizeof(AS_I2C_DEV_PATH_FMT)-2+sizeof(_STR(AS_I2C_DEV_COUNT))-1)
@@ -38,82 +38,93 @@
 
 int as_i2c_open(unsigned int i2c_id)
 {
-  char buf[AS_I2C_DEV_PATH_SIZE];
-  snprintf(buf, sizeof(buf), AS_I2C_DEV_PATH_FMT, i2c_id);
+    char buf[AS_I2C_DEV_PATH_SIZE];
 
-  return open(buf, O_RDWR);
+    snprintf(buf, sizeof(buf), AS_I2C_DEV_PATH_FMT, i2c_id);
+
+    return open(buf, O_RDWR);
 }
 
 int as_i2c_close(int fd)
 {
-  return close(fd);
+    return close(fd);
 }
 
 int as_i2c_set_slave(int fd, uint8_t addr)
 {
-  if( ioctl(fd, I2C_SLAVE_FORCE, addr) < 0 )
-    return -1;
-  return 0;
+    if (ioctl(fd, I2C_SLAVE_FORCE, addr) < 0)
+        return -1;
+
+    return 0;
 }
 
 
 int as_i2c_read(int fd, uint8_t addr, uint8_t *data, size_t n)
 {
-  struct i2c_msg msg = { addr, I2C_M_RD, n, data };
-  struct i2c_rdwr_ioctl_data rdwr = { &msg, 1 };
-  if( ioctl(fd, I2C_RDWR, &rdwr) < 0 )
-    return -1;
-  return 0;
+    struct i2c_msg msg = { addr, I2C_M_RD, n, data };
+    struct i2c_rdwr_ioctl_data rdwr = { &msg, 1 };
+
+    if (ioctl(fd, I2C_RDWR, &rdwr) < 0)
+        return -1;
+
+    return 0;
 }
 
 int as_i2c_write(int fd, uint8_t addr, const uint8_t *data, size_t n)
 {
-  struct i2c_msg msg = { addr, 0, n, (uint8_t *)data };
-  struct i2c_rdwr_ioctl_data rdwr = { &msg, 1 };
-  if( ioctl(fd, I2C_RDWR, &rdwr) < 0 )
-    return -1;
-  return 0;
+    struct i2c_msg msg = { addr, 0, n, (uint8_t *)data };
+    struct i2c_rdwr_ioctl_data rdwr = { &msg, 1 };
+
+    if (ioctl(fd, I2C_RDWR, &rdwr) < 0)
+        return -1;
+
+    return 0;
 }
 
 
 int as_i2c_read_reg(int fd, uint8_t addr, uint8_t reg, uint8_t *data, size_t n)
 {
-  // write reg
-  struct i2c_msg msg = { addr, 0, 1, &reg };
-  struct i2c_rdwr_ioctl_data rdwr = { &msg, 1 };
-  if( ioctl(fd, I2C_RDWR, &rdwr) < 0 )
-    return -1;
-  // read data
-  msg.flags = I2C_M_RD;
-  msg.len = n;
-  msg.buf = data;
-  if( ioctl(fd, I2C_RDWR, &rdwr) < 0 )
-    return -2;
-  return 0;
+    /* write reg */
+    struct i2c_msg msg = { addr, 0, 1, &reg };
+    struct i2c_rdwr_ioctl_data rdwr = { &msg, 1 };
+
+    if (ioctl(fd, I2C_RDWR, &rdwr) < 0)
+        return -1;
+    /* read data */
+    msg.flags = I2C_M_RD;
+    msg.len = n;
+    msg.buf = data;
+    if (ioctl(fd, I2C_RDWR, &rdwr) < 0)
+        return -2;
+
+    return 0;
 }
 
 int as_i2c_write_reg(int fd, uint8_t addr, uint8_t reg, const uint8_t *data, size_t n)
 {
-  uint8_t buf[n+1];
-  buf[0] = reg;
-  memcpy(buf+1, data, n);
-  return as_i2c_write(fd, addr, buf, sizeof(buf));
+    uint8_t buf[n+1];
+
+    buf[0] = reg;
+    memcpy(buf+1, data, n);
+
+    return as_i2c_write(fd, addr, buf, sizeof(buf));
 }
 
 
 int as_i2c_read_reg_byte(int fd, uint8_t addr, uint8_t reg)
 {
-  uint8_t val;
-  int ret = as_i2c_read_reg(fd, addr, reg, &val, 1);
-  if( ret < 0 )
-    return ret;
-  return val;
+    uint8_t val;
+    int ret = as_i2c_read_reg(fd, addr, reg, &val, 1);
+
+    if (ret < 0)
+        return ret;
+
+    return val;
 }
 
 int as_i2c_write_reg_byte(int fd, uint8_t addr, uint8_t reg, uint8_t val)
 {
-  uint8_t buf[2] = { reg, val };
-  return as_i2c_write(fd, addr, buf, 2);
+    uint8_t buf[2] = { reg, val };
+
+    return as_i2c_write(fd, addr, buf, 2);
 }
-
-
