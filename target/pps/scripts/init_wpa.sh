f20c526fa0a0845f1a7c0ae2901f57dbde7fbe52
Jeremie Scheer <jeremie.scheer@armadeus.com>
2010-12-21 17:15:12 +0100

    [ROOTFS] Add GPL license to PPS init file S80gpios. [TEST] Add correct indentations and carriage returns to PPS test script init_wpa.sh. [U-BOOT] Change Buildroot 2010's U-boot config file with correct rootfs image name.

--- target/pps/scripts/init_wpa.sh
+++ target/pps/scripts/init_wpa.sh
@@ -24,8 +24,9 @@ source /etc/wpa_supplicant/networkConfig
 
 if [ "$SSID" != "" -a "$PSK" != "" ]; then
 
-#create WPA configuration
-mkdir -p /etc/wpa_supplicant/
+	#create WPA configuration
+	mkdir -p /etc/wpa_supplicant/
+
 echo "ctrl_interface=/var/run/wpa_supplicant
 ctrl_interface_group=wheel
 network={
@@ -37,43 +38,37 @@ pairwise=TKIP
 psk=\"$PSK\"
 }" > /etc/wpa_supplicant/wpa_supplicant.conf
 
-#Hardware reset of wi2wi chip
-sh /usr/local/pps/scripts/reset_wi2wi.sh
+	#Hardware reset of wi2wi chip
+	sh /usr/local/pps/scripts/reset_wi2wi.sh
 
-#Load mmc and libertas modules
-modprobe mxcmmc
-sleep 1
-modprobe libertas_sdio
+	#Load mmc and libertas modules
+	modprobe mxcmmc
+	sleep 1
+	modprobe libertas_sdio
 
-#   shutdown other ports
-ifconfig usb0   down 2>/dev/null
-ifconfig eth0   down 2>/dev/null
-ifconfig usb1   down 2>/dev/null
+	#   shutdown other ports
+	ifconfig usb0   down 2>/dev/null
+	ifconfig eth0   down 2>/dev/null
+	ifconfig usb1   down 2>/dev/null
 
-#Execute Wpa Supplicant
-ifconfig eth1 up
-wpa_supplicant -ieth1 -c /etc/wpa_supplicant/wpa_supplicant.conf -Dwext -B dhclient
+	#Execute Wpa Supplicant
+	ifconfig eth1 up
+	wpa_supplicant -ieth1 -c /etc/wpa_supplicant/wpa_supplicant.conf -Dwext -B dhclient
 
-ifconfig eth1 192.168.0.251
-sleep 1
-echo -e "\nTo check the wifi port (eth1), please enter the command \"ping 192.168.0.251\" on the host PC\n"
-echo -e "\nTo restart the WPA Supplicant, before entering the command \"sh init_wpa.sh\", or to stop it, run \"sh stop_wpa.sh\"\n"
+	ifconfig eth1 192.168.0.251
+	sleep 1
+	echo -e "\nTo check the wifi port (eth1), please enter the command \"ping 192.168.0.251\" on the host PC\n"
+	echo -e "\nTo restart the WPA Supplicant, before entering the command \"sh init_wpa.sh\", or to stop it, run \"sh stop_wpa.sh\"\n"
 
 else #[ "$SSID" != "" -a "$PSK" != "" ]; print usage
 
-echo -e '\nThe variables $SSID and $PSK are not initialized in /etc/wpa_supplicant/networkConfig.
-To correctly run WPA Supplicant, you must fill these variables in like this:
-export SSID=mon_ssid
-export PSK=ma_passphrase\n'
+	echo -e '\nThe variables SSID and PSK are not initialized in /etc/wpa_supplicant/networkConfig.\nTo correctly run WPA Supplicant, you must fill these variables in like this:\nexport SSID=mon_ssid\nexport PSK=ma_passphrase\n'
 
 fi
 
 else #[ -f /etc/wpa_supplicant/networkConfig ]; print usage
 
-echo -e '\nThe file /etc/wpa_supplicant/networkConfig doesnt exist.
-To correctly run WPA Supplicant, you have to create this file and declare the variables $SSID and $PSK like this:
-export SSID=mon_ssid
-export PSK=ma_passphrase\n'
+	echo -e '\nThe file /etc/wpa_supplicant/networkConfig doesnt exist.\nTo correctly run WPA Supplicant, you have to create this file and declare the variables SSID and PSK like this:\nexport SSID=mon_ssid\nexport PSK=ma_passphrase\n'
 
 fi
 
