5087c6fa3cde785fdf6ab55c92c5ef3d1e3cf37a
Julien Boibessot <julien.boibessot@armadeus.com>
2011-03-07 10:19:13 +0100

    [BUILDROOT] Now officialy supported Buildroot version is > 2010.11 (removes BR 2008 support while we are at it).

--- Makefile
+++ Makefile
@@ -27,17 +27,10 @@ ARMADEUS_TOPDIR:=$(shell pwd)
 export ARMADEUS_TOPDIR
 
 #--- User configurable stuff:
-ifeq ($(BUILDROOT_VERSION),)
-BUILDROOT_VERSION:=20081103
-BUILDROOT_SITE:=http://downloads.sourceforge.net/armadeus
-BUILDROOT_PATCH_DIR:=$(ARMADEUS_TOPDIR)/patches/buildroot
-ARMADEUS_CONFIG_DIR:=$(BUILDROOT_DIR)/target/device/armadeus
-else
 BUILDROOT_VERSION:=2010.11
 BUILDROOT_SITE:=http://buildroot.uclibc.org/downloads
 BUILDROOT_PATCH_DIR:=$(ARMADEUS_TOPDIR)/patches/buildroot/$(BUILDROOT_VERSION)
 ARMADEUS_CONFIG_DIR:=$(BUILDROOT_DIR)/configs
-endif
 #--- End of user conf (don't touch anything below unless you know what you're doing !! ;-) )
 
 BUILDROOT_SOURCE:=buildroot-$(BUILDROOT_VERSION).tar.bz2
--- buildroot/target/device/armadeus/u-boot/u-boot.mk
+++ buildroot/target/device/armadeus/u-boot/u-boot.mk
@@ -8,14 +8,7 @@ BR2_TARGET_U_BOOT_CONFIG_BOARD=$(BR2_TARGET_UBOOT_BOARDNAME)_config
 ifndef BR2_TARGET_U_BOOT_CONFIG_HEADER_FILE
 BR2_TARGET_U_BOOT_CONFIG_HEADER_FILE=$(BOARD_PATH)/$(BOARD_NAME).h
 endif
-ifeq ($(BR2_VERSION),"0.10.0-svn")
-U_BOOT_BIN=$(BOARD_NAME)-u-boot.bin
-MKIMAGE_BINLOC:=$(STAGING_DIR)/usr/bin/mkimage
-MKIMAGE:=$(KERNEL_CROSS)mkimage
-else
-# New BR:
 U_BOOT_TARGET_BIN=$(BOARD_NAME)-u-boot.bin
-endif
 
 BR2_TARGET_UBOOT_ETH1ADDR=""
 BR2_TARGET_UBOOT_BOOTARGS=""
@@ -35,10 +28,6 @@ u-boot-distclean: u-boot-clean
 		 -C $(U_BOOT_DIR) distclean
 	rm -f $(U_BOOT_DIR)/.configured
 
-ifeq ($(BR2_VERSION),"0.10.0-svn")
-$(MKIMAGE): $(BINARIES_DIR)/$(U_BOOT_BIN)
-	ln -fs $(MKIMAGE_BINLOC) $(MKIMAGE)
-endif
 u-boot-patch: u-boot-distclean
 	-mkdir -p $(PROJECT_BUILD_DIR)/ref
 	rm -rf $(PROJECT_BUILD_DIR)/ref/u-boot-$(U_BOOT_VERSION)
@@ -53,8 +42,3 @@ u-boot-patch: u-boot-distclean
 		u-boot-$(U_BOOT_VERSION) > u-boot.patch;\
 	echo ; echo -e u-boot patch generated!;echo ;)
 
-ifeq ($(BR2_PACKAGE_LINUX),y)
-ifeq ($(BR2_VERSION),"0.10.0-svn")
-TARGETS+=$(MKIMAGE)
-endif
-endif
